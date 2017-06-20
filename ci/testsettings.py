c8218bdbec74de7a222ccfd4abdc1914c0348f73
bwhicks <benjamin.w.hicks@gmail.com>
2017-06-20 18:55:49 -0400

    Add comments for MySQL test config

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -10,16 +10,22 @@ DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
+        # Django needs to make databases in the test mysql server
         'NAME': 'root',
         'USER': 'travismep',
         'PASSWORD': '',
         'HOST': '127.0.0.1',
         'OPTIONS': {
+            # In each case, we want strict mode on to catch truncation issues
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            # Load the socket info from .my.cnf in the travis user
             'read_default_file': '~travis/.my.cnf'
         },
         'PORT': '',
         'TEST': {
+                # We also want the test databse to for utf8 and the general
+                # collation to keep case sensitive unicode searches working
+                # as we would expect on production
                 'CHARSET': 'utf8',
                 'COLLATION': 'utf8_general_ci',
                 'OPTIONS': {
