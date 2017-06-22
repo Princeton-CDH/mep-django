bf7b9aff4548ac0015fb8973c8eea69661705ac4
bwhicks <benjamin.w.hicks@gmail.com>
2017-06-22 13:43:58 -0400

    Basic Person model and tests for #6

--- mep/people/models.py
+++ mep/people/models.py
@@ -1,3 +1,46 @@
+
 from django.db import models
 
-# Create your models here.
+from mep.common.models import AliasIntegerField, DateRange, Notable
+
+
+class Person(Notable, DateRange):
+    """Model for people in the MEP dataset"""
+
+    # Identifiers
+    mep_id = models.CharField(max_length=255, blank=True)
+    first_name = models.CharField(max_length=255, blank=True)
+    last_name = models.CharField(max_length=255)
+    # QUESTION: This is listed as integer on draft 004
+    # Do we want that?
+    # May be obviated by the need for other URLs anyway
+    viaf_id = models.URLField(blank=True)
+
+    # Vital statistics
+    # QUESTION: These are aliased with the reusable logic of AliasIntegerField
+    # and the DateRange class. Worth it even though the SQL column name will
+    # be a bit less clear?
+    birth_year = AliasIntegerField(db_column='start_year',
+        blank=True, null=True)
+    death_year = AliasIntegerField(db_column='end_year',
+        blank=True, null=True)
+
+    MALE = 'M'
+    FEMALE = 'F'
+    SEX_CHOICES = (
+        ('', '----'),
+        (FEMALE, 'Female'),
+        (MALE, 'Male'),
+    )
+    sex = models.CharField(blank=True, max_length=1, choices=SEX_CHOICES)
+    # QUESTION: Do we want this to be constrained vocabulary? Probably not,
+    # but want to ask.
+    title = models.CharField(blank=True, max_length=255)
+    # TODO: Add foreign_key to profession
+
+    def __repr__(self):
+        return '<Person: %s>' % self.__dict__
+
+    def __str__(self):
+        fullname = '%s, %s' % (self.last_name, self.first_name)
+        return fullname.strip()
--- mep/people/tests.py
+++ mep/people/tests.py
@@ -1,3 +1,38 @@
+import re
+
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Person
+
+
+class TestPerson(TestCase):
+
+    def test_str(self):
+        # Last name should appear as lastname,
+        foo = Person(last_name='Foo')
+        assert str(foo) == 'Foo,'
+
+        # Full name should be Foo, Bar
+        foo_bar = Person(last_name='Foo', first_name='Bar')
+        assert str(foo_bar) == 'Foo, Bar'
+
+    def test_repr(self):
+        # Foo Bar, born 1900
+        foo = Person(last_name='Foo', first_name='Bar', birth_year=1900)
+        # Using self.__dict__ for maximum detail, so we can't count on order
+        # Testing for things that should be there and overall setup.
+        # should be of the form '<Person: {kv}>'
+        overall = re.compile(r'<Person: \{.+\}>')
+        assert re.search(overall, repr(foo))
+
+        # Sample some fields, but don't want to break this too readily with
+        # detail changes either
+        assert "'first_name': 'Bar'" in repr(foo)
+        assert "'last_name': 'Foo'" in repr(foo)
+        assert "'start_year': 1900" in repr(foo)
+        # Some fields that should get None or ''
+        assert "'end_year': None" in repr(foo)
+        assert "'sex': ''" in repr(foo)
+
+    def test_validate_years(self):
+        foo = Person(last_name='Foo', birth_year=1900)
