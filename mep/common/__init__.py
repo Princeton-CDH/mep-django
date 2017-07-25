f54a07b30eccce4c0232b6dffb4185d3aa25d062
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-07-25 13:52:51 -0400

    Clean up app config and module docstrings for common and footnotes

---
+++ mep/common/__init__.py
@@ -0,0 +1,9 @@
+'''
+Django app for shared code and functionality used
+across other applications within the project.  Currently includes
+abstract database models with common functionality or fields that are
+used by models in multiple apps within the project.
+'''
+
+
+default_app_config = 'mep.common.apps.CommonConfig'
