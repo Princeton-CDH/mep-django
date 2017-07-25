a62e5aa1836a40908248ffbf543df4f68431ab12
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-07-25 18:07:45 -0400

    Customize User display in django admin to ease user management

--- mep/common/admin.py
+++ mep/common/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
 class NamedNotableAdmin(admin.ModelAdmin):
@@ -12,3 +14,11 @@ class CollapsibleTabularInline(admin.TabularInline):
     'Django admin tabular inline with grappelli collapsible classes added'
     classes = ('grp-collapse grp-open',)
 
+
+class LocalUserAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ('is_superuser', 'is_active',
+        'last_login')
+
+
+admin.site.unregister(User)
+admin.site.register(User, LocalUserAdmin)
