3cbfc7a9e3aefc20fe521158b6fe0c74bbdd304b
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-08-03 18:06:59 -0400

    Add tests for geonames lookup view and related widgets

---
+++ mep/people/tests/test_views.py
@@ -0,0 +1,75 @@
+import json
+from unittest.mock import patch
+
+from django.http import JsonResponse
+from django.test import TestCase
+from django.urls import reverse
+
+from mep.people.admin import GeoNamesLookupWidget, MapWidget
+from mep.people.geonames import GeoNamesAPI
+from mep.people.views import GeoNamesLookup
+
+
+class TestPeopleViews(TestCase):
+
+    @patch('mep.people.views.GeoNamesAPI')
+    def test_geonames_autocomplete(self, mockgeonamesapi):
+        geo_lookup_url = reverse('people:geonames-lookup')
+        result = self.client.get(geo_lookup_url,
+            params={'q': 'amsterdam'})
+        # abbreviated sample return with fields currently in use
+        mock_response = [
+            {'name': 'New York City', 'geonameId': 5128581,
+             'countryName': 'USA', 'lat': 40.71427, 'lng': -74.00597}
+        ]
+        mockgeonamesapi.return_value.search.return_value = mock_response
+        # patch in real uri from id logic
+        mockgeonamesapi.return_value.uri_from_id = GeoNamesAPI.uri_from_id
+
+        result = self.client.get(geo_lookup_url,
+            params={'q': 'new york'})
+        assert isinstance(result, JsonResponse)
+        assert result.status_code == 200
+        # decode response to inspect
+        data = json.loads(result.content.decode('utf-8'))
+        # inspect constructed result
+        item = data['results'][0]
+        assert item['text'] == 'New York City, USA'
+        assert item['name'] == 'New York City'
+        assert item['lat'] == mock_response[0]['lat']
+        assert item['lng'] == mock_response[0]['lng']
+        assert item['id'] == \
+            GeoNamesAPI.uri_from_id(mock_response[0]['geonameId'])
+
+    def test_get_label(self):
+        geo_lookup = GeoNamesLookup()
+        item = {'name': 'New York City', 'countryName': 'USA'}
+        # country code used if available
+        assert geo_lookup.get_label(item) == 'New York City, USA'
+        del item['countryName']
+        # and just name, if no country is available
+        assert geo_lookup.get_label(item) == 'New York City'
+
+
+class TestGeonamesLookupWidget(TestCase):
+
+    def test_render(self):
+        widget = GeoNamesLookupWidget()
+        # no value set - should not error
+        rendered = widget.render('place', None, {'id': 'place'})
+        assert '<p><a id="geonames_uri" target="_blank" href=""></a></p>' \
+            in rendered
+        # uri value set - should be included in generated link
+        uri = 'http://sws.geonames.org/2759794/'
+        rendered = widget.render('place', uri, {'id': 'place'})
+        assert '<a id="geonames_uri" target="_blank" href="%(uri)s">%(uri)s</a>' \
+            % {'uri': uri} in rendered
+
+
+class TestMapWidget(TestCase):
+
+    def test_render(self):
+        widget = MapWidget()
+        # no value set - should not error
+        rendered = widget.render('place', None, {'id': 'place'})
+        assert '<div id="geonames_map"></div>' in rendered
