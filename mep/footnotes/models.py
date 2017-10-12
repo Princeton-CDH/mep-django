2da7ed211d8bf1bca25aad37359fb88effdcf557
rlskoeser <rebecca.s.koeser@princeton.edu>
2017-10-12 17:53:38 -0400

    Clean up redundant imports

--- mep/footnotes/models.py
+++ mep/footnotes/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-
-# Create your models here.
-from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
 
