e9a626a2b0655e00e3b5041e8c63963c106df3d4
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-10-16 15:56:12 -0400

    Add ansible deploy script for production (Draft for QA)

--- mep/accounts/xml_models.py
+++ mep/accounts/xml_models.py
@@ -1,12 +1,12 @@
 # coding=utf-8
 import logging
+import pendulum
+from calendar import monthrange
 import re
 from django.db.utils import IntegrityError
 from django.core.exceptions import ObjectDoesNotExist
 from eulxml import xmlmap
 from viapy.api import ViafEntity
-from datetime import datetime, timedelta
-import sys
 from mep.accounts.models import Account, Subscribe
 from mep.people.models import Person
 
@@ -51,118 +51,180 @@ class XmlEvent(TeiXmlObject):
         't:p/t:measure[@type="reimbursement"]/@quantity'
     )
 
-    def to_db_event(self, date):
+    def _is_int(self, value):
+        try:
+            int(value)
+        except ValueError:
+            return False
+        return True
 
+    def _normalize_dates(self, common_dict):
         # Check to see that durations are monthly, if not flag them
         # to test and handle
         if self.duration_unit and self.duration_unit not in ['month', 'day']:
             raise ValueError('Unexpected duration_unit on %s' % date)
 
-        # clean duration quantity weirdnesses
-        # i.e., #davey 1929-02-22 with '1/4' as quantity
-        if self.duration_quantity and '/' in self.duration_quantity:
-            num, div = self.duration_quantity.split('/')
-            self.duration_quantity = float(int(num)/int(div))
+        # get a pendulum instance for month timedeltas
+        pd = pendulum.date.instance(common_dict['start_date'])
 
-        # Create a common dict
+        # - simple case
+        if self.duration_unit == 'month' and \
+                self._is_int(self.duration_quantity):
+            common_dict['duration'] = int(self.duration_quantity)
+            common_dict['end_date'] = pd.add(months=common_dict['duration'])
+        # - duration given as quarter or half
+        if self.duration_unit == 'month' and \
+                not self._is_int(self.duration_quantity):
+            common_dict['duration'] = float(self.duration_quantity)
+            # .25 seems to equal one week, or seven days. A half month
+            # presumably means to middle of next calendar month, so using 15
+            # as nearest approximate
+            common_dict['end_date'] = pd.add(
+                days=round(common_dict['duration']*29)
+            )
+        # - duration given as days
+        elif self.duration_unit == 'days':
+            common_dict['duration'] = float(int(self.duration_quantity) / 28)
+            common_dict['end_date'] = pd.add(days=int(self.duration_quantity))
+        else:
+            common_dict['duration'] = None
+            common_dict['end_date'] = None
+
+        return common_dict
+
+    def _normalize(self, e_date):
+        '''Return a version of the XMLEvent object with irregularities
+        cleaned up.
+        :param e_date: Date of event as a `!`
+        :type e_date: str.
+        :returns: tuple of event type (str.), dict of values for event object,
+        :class:`mep.people.models.Person` object, and
+        :class:`mep.accounts.models.Account` object.
+        '''
+
+        # Create a common dict for base of different event types
         common_dict = {
-            'start_date': date,
-            'end_date': (date +
-                         timedelta(weeks=float(self.duration_quantity) * 4)
-                         if self.duration_quantity else date),
-            'notes': ''
+            'start_date': e_date,
+            'notes': '',
         }
 
+        common_dict = self._normalize_dates(common_dict)
 
-        # Map xml events to class names
+        # Map xml events to class names for use with add_event()
         xml_db_mapping = {
             'subscription': 'subscribe',
             'supplement': 'subscribe',
             'reimbursement': 'reimbursement',
             'borrow': 'borrow',
-            'renewal': 'subscribe'
+            'renewal': 'subscribe',
+            'other': 'event',
         }
+
         # check for unhandled event types
         if self.e_type not in xml_db_mapping:
             raise ValueError('Unexpected e_type on %s' % date)
         etype = xml_db_mapping[self.e_type]
         # Map XML currency to database abbreviations
+        # So far, logbooks seem only to list francs
         xml_currency_mapping = {
-            'franc': 'FRF'
+            'franc': 'FRF',
         }
+
         currency = ''
-
-        # handle dud reimbursement units -- i.e., #kran 1929-11-02
-        # where the unit is month and should clearly be franc, but note them
-        # in case the most likely currency (franc) is wrong
-        if self.deposit_unit not in ['franc', 'dollar']:
-            self.deposit_unit = 'franc'
-            common_dict['notes'] = 'Uncertain currency\n'
-
         if self.deposit_unit:
             currency = xml_currency_mapping[self.deposit_unit]
         elif self.price_unit:
             currency = xml_currency_mapping[self.price_unit]
         elif self.reimbursement_unit:
             currency = xml_currency_mapping[self.reimbursement_unit]
+        common_dict['currency'] = currency
 
         # Get or create person and account
+        # The logic here handles the accounts that (at least in the logbooks)
+        # don't actually include a person to have a mep_id to associate or
+        # even a person name.
         mep_id = self.mepid.strip('#') if self.mepid else ''
         person = ''
         account = None
         if not mep_id:
-            account, created = Account.objects.get_or_create(id=int(re.sub('-', '', str(date))))
+            # Giving these special id's so that they're flagged via ID
+            # as it's the only field that isn't a M2M
+            account, created = Account.objects.get_or_create(
+                id=int(re.sub('-', '', str(e_date)))
+            )
+            common_dict['notes'] += (
+                '!!!Event irregularity!!!\n'
+                'No person is associated with this account via <persName>'
+            )
         else:
             person, created = Person.objects.get_or_create(mep_id=mep_id)
             if created:
+                # Create a stub record if they weren't in the personogoraphy
+                # import
                 person.name = self.name.strip()
                 person.save()
-            account, created = Account.objects.get_or_create(persons__in=[person])
-
-        if etype == 'subscribe':
-            common_dict['duration'] = (
-                # only two units are months or days
-                self.duration_quantity if self.duration_unit == 'month' else
-                (int(self.duration_quantity) / 28) if self.duration_quantity else
-                None
+            account, created = Account.objects.get_or_create(
+                persons__in=[person]
             )
-            common_dict['volumes'] = self.frequency_quantity
-            common_dict['price_paid'] = self.price_quantity
-            common_dict['deposit'] = self.deposit_quantity
-            common_dict['currency'] = currency
+        return (etype, common_dict, person, account)
 
-            if self.e_type == 'subscribe' and (not common_dict['duration'] or
-                                               not common_dict['price_paid'] or
-                                               not common_dict['volumes']):
-                common_dict['notes'] += (
-                    'Subscribe irregularity:\n'
-                    'Duration: %s\n'
-                    'Volumes: %s\n'
-                    'Price Paid: %s\n'
-                    '%s on %s\n'
-                    'Please check to see if this event can be clarified.\n'
-                    % (self.duration_quantity, self.frequency_quantity,
-                       self.price_quantity, self.mep_id, self.date)
-                )
+    def _parse_subscribe(self, common_dict):
+        '''Parse fields from a dictionary for
+        :class:`mep.accounts.models.Subscribe` and its sub modifications.'''
+
+
+        common_dict['volumes'] = self.frequency_quantity
+        common_dict['price_paid'] = self.price_quantity
+        common_dict['deposit'] = self.deposit_quantity
+
+        if self.e_type == 'subscription' and (not common_dict['duration'] or
+                                              not common_dict['price_paid'] or
+                                              not common_dict['volumes']):
+            common_dict['notes'] += (
+                '!!!Event irregularity!!!\n'
+                'Duration: %s\n'
+                'Volumes: %s\n'
+                'Price Paid: %s\n'
+                '%s on %s\n'
+                'Please check to see if this event can be clarified.\n'
+                % (self.duration_quantity, self.frequency_quantity,
+                   self.price_quantity, self.mepid, common_dict['start_date'])
+            )
 
             if self.e_type == 'supplement':
                 common_dict['modification'] = Subscribe.SUPPLEMENT
+
             if self.e_type == 'renewal':
                 common_dict['modification'] = Subscribe.RENEWAL
+        # clear empty keys
+        return {k: v for k, v in common_dict.items() if v}
 
+    def to_db_event(self, e_date):
+        '''Parse a :class:`XMLEvent` to :class:`mep.accounts.models.Event`
+        or subclass.
+            :param e_date: A date object representing the event date
+            :type e_date: datetime.date
+        '''
+        # normalize the event
+        etype, common_dict, person, account = self._normalize(e_date)
+        # This database type encompasses supplements and renewals
+        if etype == 'subscribe':
+            common_dict = self._parse_subscribe(common_dict)
+        # Reimbursement is a subclass that doesn't warrant its own
+        # private function
         if etype == 'reimbursement':
             common_dict['price'] = self.price_quantity if self.price_quantity \
                                    else self.reimbursement_quantity
-            common_dict['currency'] = currency
             if not common_dict['price']:
                 common_dict['notes'] += (
-                    'Reimbursement irregularity:\n'
+                    '!!!Event irregularity!!!\n'
                     'Price is missing\n'
-
                 )
+        # If there is a person, add them to an account
         if person:
             account.persons.add(person)
             account.save()
+        # Call method to create and save the event
         account.add_event(etype, **common_dict)
 
 
