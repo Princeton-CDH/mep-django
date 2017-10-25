7eaead77e8643e40eaee78aece289c4ca0fb132c
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2017-10-25 13:13:28 -0400

    Add search by mep_id for reimbursements admin enchancement for #42

--- mep/accounts/admin.py
+++ mep/accounts/admin.py
@@ -55,7 +55,7 @@ class ReimbursementAdmin(admin.ModelAdmin):
     fields = ('account', 'price', 'currency', 'start_date', 'end_date', 'notes')
     list_display = ('account', 'price', 'currency', 'start_date', 'end_date',)
     list_filter = ('currency',)
-    search_fields = ('account__persons__name',)
+    search_fields = ('account__persons__name', 'account__persons_mepid')
 
 
 class SubscribeAdmin(admin.ModelAdmin):
