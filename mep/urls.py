62e9b2ecbef0cec05cffbab000cef044fd2eeda9
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-02-08 16:12:45 -0500

    Add mezzanine and harmonize with stock grappelli

--- mep/urls.py
+++ mep/urls.py
@@ -13,18 +13,30 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic.base import RedirectView
+import mezzanine.urls
 
 urlpatterns = [
     # for now, since there is not yet any public-facing site,
     # redirect base url to admin index page
-    url(r'^$', RedirectView.as_view(pattern_name='admin:index'), name='site-index'),
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', RedirectView.as_view(pattern_name='admin:index'), name='home'),
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^accounts/', include('pucas.cas_urls')),
     url(r'^viaf/', include('viapy.urls', namespace='viaf')),
     url(r'^', include('mep.people.urls', namespace='people')),
     url(r'^', include('mep.accounts.urls', namespace='accounts')),
+
+    # content pages managed by mezzanine
+    url(r'^', include(mezzanine.urls))
 ]
+
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
