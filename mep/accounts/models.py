de98729c4075ceccbf5d76c62ddaaac1df5a905f
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-02-12 13:43:38 -0500

    Add property to access subscriptions events for an account #60

--- mep/accounts/models.py
+++ mep/accounts/models.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from dateutil.relativedelta import relativedelta
 from django.core.exceptions import ObjectDoesNotExist
 from django.core.validators import ValidationError
@@ -67,6 +68,10 @@ class Account(models.Model):
             # if no end date is present, return start date
             return evt.end_date or evt.start_date
 
+    @property
+    def subscription_set(self):
+        '''associated subscription events, as queryset of :class:`Subscription`'''
+        return Subscription.objects.filter(account_id=self.id)
 
     def list_addresses(self):
         '''List :class:`mep.people.models.Address` instances associated with
