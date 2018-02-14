40a54405c931171bc2be2778755ac810bded4896
Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
2018-02-14 15:02:13 -0500

    Javascript to display edit link for auto-completed person #56 (#71)
    * Javascript to display edit link for auto-completed person #56
    
    * Downgrade fixme to a note since we can live with the constraints

--- mep/accounts/admin.py
+++ mep/accounts/admin.py
@@ -242,6 +242,9 @@ class AccountAdmin(admin.ModelAdmin):
     fields = ('persons',)
     inlines = [AccountAddressInline, SubscriptionInline, ReimbursementInline]
 
+    class Media:
+        js = ("admin/js/person-editlink.js",)
+
 
 class SubscriptionTypeAdmin(NamedNotableAdmin):
     list_display = ('name', 'notes')
