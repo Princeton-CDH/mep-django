0a7ff99c871f451a3867a60ba22ea5b9a5553508
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-04-17 17:41:29 -0400

    Make bought flag visible & filterable in admin

--- mep/accounts/admin.py
+++ mep/accounts/admin.py
@@ -275,11 +275,12 @@ class BorrowAdminForm(forms.ModelForm):
 
 class BorrowAdmin(admin.ModelAdmin):
     form = BorrowAdminForm
-    list_display = ('account', 'item', 'display_start_date', 'display_end_date')
+    list_display = ('account', 'item', 'display_start_date', 'display_end_date',
+        'bought')
     date_hierarchy = 'start_date'
     search_fields = ('account__persons__name', 'account__persons__mep_id',
         'notes', 'item__title', 'item__notes')
-
+    list_filter = ('bought',)
 
 
 admin.site.register(Subscription, SubscriptionAdmin)
