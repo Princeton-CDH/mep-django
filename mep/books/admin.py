f230f2145c318cfc02ca9dfd14fce5e644d99f5d
Nick Budak <nbudak@princeton.edu>
2018-04-26 09:56:37 -0400

    Add borrow counts and link to item admin (#94)

--- mep/books/admin.py
+++ mep/books/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django import forms
+from django.db.models import Count
+from django.utils.html import format_html
+from django.core.urlresolvers import reverse
 
 from mep.books.models import Item, Creator, CreatorType
 from mep.common.admin import CollapsibleTabularInline
@@ -23,12 +26,12 @@ class ItemCreatorInline(CollapsibleTabularInline):
 
 
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['mep_id', 'title', 'author_list', 'notes']
+    list_display = ['mep_id', 'title', 'author_list', 'notes', 'borrow_count']
     inlines = [ItemCreatorInline]
     search_fields = ('mep_id', 'title', 'notes', 'creator__person__name')
     fieldsets = (
         ('Basic metadata', {
-            'fields': ('title', 'mep_id', 'year')
+            'fields': ('title', 'year', 'mep_id', 'borrow_count')
         }),
         ('Additional metadata', {
             'fields': (
@@ -39,7 +42,26 @@ class ItemAdmin(admin.ModelAdmin):
             )
         })
     )
-    readonly_fields = ('mep_id',)
+    readonly_fields = ('mep_id', 'borrow_count')
+
+    def get_queryset(self, request):
+        '''Annotate the queryset with the number of borrows for sorting'''
+        qs = super(ItemAdmin, self).get_queryset(request)
+        qs = qs.annotate(Count('borrow'))
+        return qs
+
+    def borrow_count(self, obj):
+        '''Display the borrow count as a link to view associated borrows'''
+        return format_html(
+            '<a href="{}">{}</a>',
+            # TODO make this more elegant?
+            reverse('admin:accounts_borrow_changelist') + '?item__id__exact=' + str(obj.id),
+            obj.borrow_count # this is the property, not the annotation
+        )
+
+    # use the annotated queryset value to make the field sortable
+    borrow_count.admin_order_field = 'borrow__count'
+
 
 class CreatorTypeAdmin(admin.ModelAdmin):
     model = CreatorType
---
+++ mep/books/tests/test_books_views.py
@@ -0,0 +1,45 @@
+from django.core.urlresolvers import reverse
+from django.test import TestCase
+
+from mep.books.models import Item
+
+
+class TestBooksViews(TestCase):
+
+    def test_item_autocomplete(self):
+        url = reverse('books:item-autocomplete')
+        res = self.client.get(url)
+
+        # getting the view returns 200
+        assert res.status_code == 200
+        data = res.json()
+        # there is a results list in the JSON
+        assert 'results' in data
+        # it is empty because there are no accounts or query
+        assert not data['results']
+
+        # - test basic search and sort
+
+        # search by title
+        item1 = Item.objects.create(title='Poems Two Painters', mep_id='mep:01',
+            notes='Author: Knud Merrild')
+        item2 = Item.objects.create(title='Collected Poems', mep_id='mep:02')
+        res = self.client.get(url, {'q': 'poems'})
+        data = res.json()
+        assert res.status_code == 200
+        assert 'results' in data
+        assert len(data['results']) == 2
+        assert data['results'][0]['text'] == item2.title
+        assert data['results'][1]['text'] == item1.title
+
+        # search by note text
+        res = self.client.get(url, {'q': 'knud'})
+        data = res.json()
+        assert len(data['results']) == 1
+        assert data['results'][0]['text'] == item1.title
+
+        # search by mep id
+        res = self.client.get(url, {'q': 'mep:02'})
+        data = res.json()
+        assert len(data['results']) == 1
+        assert data['results'][0]['text'] == item2.title
