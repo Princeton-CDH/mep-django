e79a87c77066766e0e86c1c0824d73bde98d7ac5
rlskoeser <rebecca.s.koeser@princeton.edu>
2018-09-18 16:17:29 -0400

    Test logic for reporting on account event gaps

--- mep/accounts/tests/test_accounts_commands.py
+++ mep/accounts/tests/test_accounts_commands.py
@@ -1,5 +1,5 @@
 from io import StringIO
-from datetime import datetime, timedelta
+from datetime import date, timedelta
 
 from dateutil.relativedelta import relativedelta
 from django.test import TestCase
@@ -45,11 +45,11 @@ class TestReportTimegaps(TestCase):
 
         # two events close together, smaller than the gap = still no gaps
         event1 = Event.objects.create(
-            account=account, start_date=datetime(1943, 1, 1),
-            end_date=datetime(1943, 2, 1))
+            account=account, start_date=date(1943, 1, 1),
+            end_date=date(1943, 2, 1))
         event2 = Event.objects.create(
-            account=account, start_date=datetime(1943, 3, 1),
-            end_date=datetime(1943, 4, 1))
+            account=account, start_date=date(1943, 3, 1),
+            end_date=date(1943, 4, 1))
         assert self.cmd.find_gaps(account, timedelta(days=30)) == []
 
         # if first event end date is not found, start date should be used
@@ -93,11 +93,54 @@ class TestReportTimegaps(TestCase):
         # borrow events with partial dates are currently ignored
         account.event_set.all().delete()
         Event.objects.create(
-            account=account, start_date=datetime(1943, 1, 1),
-            end_date=datetime(1943, 2, 1))
+            account=account, start_date=date(1943, 1, 1),
+            end_date=date(1943, 2, 1))
         borrow = Borrow.objects.create(account=account)
         borrow.partial_start_date = '1950-01'
         borrow.save()
         # gap is significant, but partial dates are skipped
         assert self.cmd.find_gaps(account, timedelta(days=61)) == []
+
+    def test_report_gap_details(self):
+        # shouldn't be called with empty gap list normally, but shouldn't error
+        max_gap, msg = self.cmd.report_gap_details([])
+        assert max_gap == 0
+        assert msg == ''
+
+        account = Account.objects.create()
+        # add two events to check simple case of gap and message
+        event1 = Event.objects.create(
+            account=account, start_date=date(1943, 1, 1),
+            end_date=date(1943, 2, 1))
+        delta_days = 20
+        event2 = Event.objects.create(
+            account=account, start_date=event1.end_date + timedelta(days=delta_days))
+        max_gap, msg = self.cmd.report_gap_details([(event1, event2)])
+        # largest gap reported in days
+        assert max_gap == delta_days
+        # message includes days and event type
+        # (generic events currently display as just 'generic', but there are very few)
+        assert msg == '{} days between {}/{} Generic and {}/?? Generic'.format(
+            delta_days, event1.start_date.isoformat(), event1.end_date.isoformat(),
+            event2.start_date.isoformat()
+        )
+
+        # if multiple events, largest gap is returned
+        delta2_days = 25
+        event3 = Event.objects.create(
+            account=account, start_date=event2.start_date + timedelta(days=delta2_days))
+        max_gap, msg = self.cmd.report_gap_details([(event1, event2), (event2, event3)])
+        assert max_gap == delta2_days
+        # message reports on both gaps
+        assert '{} days between'.format(delta_days) in msg
+        assert '{} days between'.format(delta2_days) in msg
+
+        # event type should be reported in message
+        borrow = Borrow.objects.create(account=account)
+        borrow.partial_start_date = (event3.start_date + timedelta(days=30)).isoformat()
+        max_gap, msg = self.cmd.report_gap_details([(event3, borrow)])
+        assert '{}/?? Borrow'.format(borrow.partial_start_date) in msg
+
+
+
 
