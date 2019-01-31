c9e6b8a76f3777bba39c888fcf53dcf244ebbba3
Nick Budak <thatbudakguy@gmail.com>
2019-01-31 14:18:17 -0500

    gracefully degrading nav menu

--- js/src/main.js
+++ js/src/main.js
@@ -1,3 +1,7 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('page loaded')
-})
+import { openMainMenuButton, closeMainMenuButton } from './modules/elements'
+import { onMainMenuOpen, onMainMenuClose } from './modules/handlers'
+
+/* page setup */
+
+openMainMenuButton.then(element => element.onclick = onMainMenuOpen)
+closeMainMenuButton.then(element => element.onclick = onMainMenuClose)
