0e471ff82496632276249e4f94e28b1a58c5f0d8
Nick Budak <thatbudakguy@gmail.com>
2019-02-07 14:11:18 -0500

    move webpack settings to after localsettings load

--- mep/settings.py
+++ mep/settings.py
@@ -199,15 +199,6 @@ SITE_ID = 1
 # use grappelli custom dashboard for consistent admin menu ordering
 GRAPPELLI_INDEX_DASHBOARD = 'mep.dashboard.CustomIndexDashboard'
 
-# Django webpack loader
-WEBPACK_LOADER = {
-    'DEFAULT': {
-        'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'static/',
-        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
-    }
-}
-
 ##################
 # LOCAL SETTINGS #
 ##################
@@ -233,17 +224,11 @@ if os.path.exists(f):
     exec(open(f, "rb").read())
 
 
-## Mezzanine dynamic settings
-
-# set_dynamic_settings() will rewrite globals based on what has been
-# defined so far, in order to provide some better defaults where
-# applicable. We also allow this settings module to be imported
-# without Mezzanine installed, as the case may be when using the
-# fabfile, where setting the dynamic settings below isn't strictly
-# required.
-try:
-    from mezzanine.utils.conf import set_dynamic_settings
-except ImportError:
-    pass
-else:
-    set_dynamic_settings(globals())
+# Django webpack loader
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': not DEBUG,
+        'BUNDLE_DIR_NAME': 'static/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+    }
+}
