08e4504e2ce69894e27bde8efbc43c8a038f2ed1
Nick Budak <thatbudakguy@gmail.com>
2019-02-14 13:04:36 -0500

    move webpack output to bundles/ and source media to srcmedia/

--- srcmedia/test/unit/FacetChoice.test.js
+++ srcmedia/test/unit/FacetChoice.test.js
@@ -1,6 +1,6 @@
 import { mount } from '@vue/test-utils'
 
-import FacetChoice from '../../src/components/FacetChoice'
+import FacetChoice from '../../js/components/FacetChoice'
 
 describe('FacetChoice', () => {
 
