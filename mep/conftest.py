91c67ff33c41d1fea1b478dc1cb00c140655551b
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-02-14 17:17:34 -0500

    Require newer version of pytest for consistent test plugin behavior

--- mep/conftest.py
+++
@@ -1,5 +0,0 @@
-# pytest configuration and local fixtures
-
-# use parasol plugin for test solr config
-# causes "Plugin already registered" error on travis...
-#pytest_plugins = "parasol.pytest_plugin"
