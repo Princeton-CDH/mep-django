25617f0a088d329daf9e1c2e4a81d98913c17510
Nick Budak <thatbudakguy@gmail.com>
2019-02-28 16:20:31 -0500

    update django-webpack-loader settings

--- mep/settings.py
+++ mep/settings.py
@@ -228,7 +228,10 @@ if os.path.exists(f):
 WEBPACK_LOADER = {
     'DEFAULT': {
         'CACHE': not DEBUG,
-        'BUNDLE_DIR_NAME': 'static/',
+        'BUNDLE_DIR_NAME': 'bundles/', # must end with slash
         'STATS_FILE': os.path.join(BASE_DIR, 'webpack-stats.json'),
+        'POLL_INTERVAL': 0.1,
+        'TIMEOUT': None,
+        'IGNORE': [r'.+\.hot-update.js', r'.+\.map']
     }
 }
