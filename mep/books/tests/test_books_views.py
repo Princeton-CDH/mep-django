f5c68697ab832df11a39f163058ad1a8e2859c30
Nick Budak <thatbudakguy@gmail.com>
2019-03-20 17:13:08 -0400

    add tests for multi-author work

--- mep/books/tests/test_books_views.py
+++ mep/books/tests/test_books_views.py
@@ -57,20 +57,21 @@ class TestBooksViews(TestCase):
         assert data['results'][0]['text'] == item2.title
 
 class TestItemListView(TestCase):
-    fixtures = ['sample_items.json']
+    fixtures = ['sample_items.json', 'multi_creator_item.json']
 
-    def test_list(self):
+    def setUp(self):
         Item.index_items(Item.objects.all())
         # give time for index to take effect
         time.sleep(10)
 
+    def test_list(self):
         url = reverse('books:books-list')
         response = self.client.get(url)
 
         # should display all items in the database
         items = Item.objects.all()
         assert response.context['items'].count() == items.count()
-        self.assertContains(response, '%d results' % items.count())
+        self.assertContains(response, '%d total results' % items.count())
         for item in items:
             self.assertContains(response, item.title)
             self.assertContains(response, item.year)
@@ -80,5 +81,18 @@ class TestItemListView(TestCase):
         # NOTE publishers display is designed but data not yet available
 
     def test_many_authors(self):
-        pass
+        url = reverse('books:books-list')
+        response = self.client.get(url)
 
+        novelists = Item.objects.get(pk=4126)
+
+        # multi-author item should show first three authors
+        self.assertContains(response, novelists.authors.first())
+        self.assertContains(response, novelists.authors[1])
+        self.assertContains(response, novelists.authors[2])
+
+        # other authors should be hidden
+        self.assertNotContains(response, novelists.authors[3])
+
+        # should show "...x more authors" text
+        self.assertContains(response, '... 16 more authors')
