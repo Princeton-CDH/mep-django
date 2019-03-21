6167fbdbbf111158c29505512487521a732fe8fd
Rebecca Sutton Koeser <rlskoeser@users.noreply.github.com>
2019-03-21 16:36:01 -0400

    Add alpha pagination to members list (#156)
    * Adapt alpha_pagelabels from emory findingaids code
    
    * Generate pagination labels in members list view and use in template
    
    * Only show next/prev links when there is more than one page of results
    
    * Tests for member list pagination
    
    * Try configuring travis-ci to use more aggressive solr commit
    
    * reconcile styles & fix a z-index issue
    
    * ignore input-within-form rule in pa11y

--- mep/common/utils.py
+++ mep/common/utils.py
@@ -1,3 +1,6 @@
+from collections import OrderedDict
+import re
+
 from django.conf import settings
 from django.contrib.sites.models import Site
 
@@ -31,3 +34,77 @@ def absolutize_url(local_url, request=None):
         root = root.rstrip('/')
 
     return root + local_url
+
+
+def alpha_pagelabels(paginator, objects, attr_meth):
+    """Generate abbreviated, alphabetical page labels for pagination items.
+    Label format should be something like 'Ab - Ad', 'Ard - Art'.
+    :param paginator: a django paginator
+    :param objects: the complete list of objects paginated by the paginator
+    :param attr_meth: method or lambda to retrieve the attribute on the
+        object that shoudl be used for page labels
+    :returns: :class:`~collections.OrderedDict` where keys are page
+        numbers and values are page labels
+    """
+    # adapted from Emory findingaids
+    # https://github.com/emory-libraries/findingaids/blob/master/findingaids/fa/utils.py#L163
+
+    page_labels = OrderedDict()
+    labels = []
+
+    if paginator.count <= 1:
+        # if there is not enough content to paginate, bail out
+        return page_labels
+
+    # get all labels for first and last objects on each page
+    for i in range(paginator.num_pages):
+        page = paginator.page(i + 1)  # paginator page is 1-based
+        # get objects at start & end of each page (paginator index also 1-based)
+        labels.append(attr_meth(objects[page.start_index() - 1]))
+        # don't go beyond the end of the actual number of objects
+        end_index = min(page.end_index() - 1, paginator.count)
+        # add end label only if not the same as first (e.g., page of a single item)
+        if page.start_index() - 1 != end_index:
+            labels.append(attr_meth(objects[end_index]))
+
+    # abbreviate labels so they are as short as possible but distinct from
+    # preceding and following labels
+    abbreviated_labels = []
+    # iterate over all labels and generate the shortest distinct version
+    for i, label in enumerate(labels):
+        next_label = labels[i + 1] if i + 1 < len(labels) else ''
+        prev_label = labels[i - 1] if i > 0 else ''
+
+        # In the rare case that two labels are *exactly* the same,
+        # just add the full label.
+        if label in (next_label, prev_label):
+            abbreviated_labels.append(label)
+            continue
+
+        # iterate through the label one letter at a time
+        # to determine the minimum length needed to differentiate
+        # start with one letter, go up to full length of the label if necessary
+        abbr = ''
+        for j, char in enumerate(label, 1):
+            abbr += char   # or abbr = label[:j]
+            # if abbreviation is different from neighboring labels
+            # at the current length, then use it
+            if abbr not in (next_label[:j], prev_label[:j]):
+                break
+            # if we don't break before the loop finishes, use
+            # the whole label
+
+        abbreviated_labels.append(abbr)
+
+    # iterate over the abbreviate labels in pairs and generate page labels
+    for i in range(0, len(abbreviated_labels), 2):
+        page_index = int((i + 2) / 2)
+        try:
+            page_labels[page_index] = '%s - %s' % (abbreviated_labels[i].strip(),
+                                                   abbreviated_labels[i+1].strip())
+        except IndexError:
+            # paginator was not created with orphan protection,
+            # it's possible we could get a single item at the end
+            page_labels[page_index] = abbreviated_labels[i].strip()
+
+    return page_labels
