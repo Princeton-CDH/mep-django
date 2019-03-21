0d729a2eac011d46f47302619c84428b55f810e9
Nick Budak <thatbudakguy@gmail.com>
2019-03-21 09:44:08 -0400

    Books browse (#189)
    * create basic books list view
    
    * add item search result & list templates
    
    * generalize search result styles
    
    * use solr index for book browse
    
    * add multi-creator item fixture
    
    * add tests for multi-author work
    
    * Remove fixture_magic from installed apps

--- mep/books/tests/test_books_views.py
+++ mep/books/tests/test_books_views.py
@@ -1,11 +1,8 @@
-import csv
-from io import StringIO
-import re
+import time
 
 from django.contrib.auth import get_user_model
 from django.core.urlresolvers import reverse
 from django.test import TestCase
-from django.utils.timezone import now
 
 from mep.books.models import Item
 
@@ -58,3 +55,44 @@ class TestBooksViews(TestCase):
         data = res.json()
         assert len(data['results']) == 1
         assert data['results'][0]['text'] == item2.title
+
+class TestItemListView(TestCase):
+    fixtures = ['sample_items.json', 'multi_creator_item.json']
+
+    def setUp(self):
+        Item.index_items(Item.objects.all())
+        # give time for index to take effect
+        time.sleep(10)
+
+    def test_list(self):
+        url = reverse('books:books-list')
+        response = self.client.get(url)
+
+        # should display all items in the database
+        items = Item.objects.all()
+        assert response.context['items'].count() == items.count()
+        self.assertContains(response, '%d total results' % items.count())
+        for item in items:
+            self.assertContains(response, item.title)
+            self.assertContains(response, item.year)
+            # NOTE should link to item detail page; not yet implemented
+            # self.assertContains(response, item.get_absolute_url())
+
+        # NOTE publishers display is designed but data not yet available
+
+    def test_many_authors(self):
+        url = reverse('books:books-list')
+        response = self.client.get(url)
+
+        novelists = Item.objects.get(pk=4126)
+
+        # multi-author item should show first three authors
+        self.assertContains(response, novelists.authors.first())
+        self.assertContains(response, novelists.authors[1])
+        self.assertContains(response, novelists.authors[2])
+
+        # other authors should be hidden
+        self.assertNotContains(response, novelists.authors[3])
+
+        # should show "...x more authors" text
+        self.assertContains(response, '... 16 more authors')
