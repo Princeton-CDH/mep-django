a71c46a6f31361cbffccdeeaccc39b09051be19a
Nick Budak <thatbudakguy@gmail.com>
2019-04-02 17:28:12 -0400

    create PageControls component for sticky events

---
+++ srcmedia/ts/components/PageControls.ts
@@ -0,0 +1,60 @@
+/**
+ * A sort/pagination control component that applies a css class when it's stuck
+ * to the top of the page.
+ */
+export default class PageControls {
+    element: HTMLElement
+    position: number = 0
+    ticking: boolean = false
+    stuck: boolean = false
+    top: number = 0
+
+    constructor(element: HTMLElement) {
+        this.element = element
+        // Get the top property
+        let top = window
+            .getComputedStyle(this.element)
+            .getPropertyValue('top')
+        // Convert a css value like '50px' to integer pixels
+        if (top) this.top = parseFloat(top)
+        // Bind a scroll event listener to check if sticking
+        window.addEventListener('scroll', this.scroll.bind(this))
+    }
+
+    /**
+     * Event handler for scroll events that checks if the element is sticking
+     * to its specified top value. Uses requestAnimationFrame to avoid making
+     * needless calls and improve performance.
+     */
+    scroll(): void {
+        this.position = window.scrollY
+        if (!this.ticking) {
+            window.requestAnimationFrame(() => {
+                if (this.element.getBoundingClientRect().top == this.top && !this.stuck) {
+                    this.stick()
+                }
+                else if (this.element.getBoundingClientRect().top != this.top && this.stuck) {
+                    this.unstick()
+                }
+                this.ticking = false
+            })
+            this.ticking = true
+        }
+    }
+
+    /**
+     * Applies the 'stuck' class to indicate the element is currently sticking.
+     */
+    stick(): void {
+        this.stuck = true
+        this.element.classList.add('stuck')
+    }
+
+    /**
+     * Removes the 'stuck' class to indicate the element is no longer sticking.
+     */
+    unstick(): void {
+        this.stuck = false
+        this.element.classList.remove('stuck')
+    }
+}
