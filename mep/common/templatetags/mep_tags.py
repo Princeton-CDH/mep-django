580731013d13d5ff2cb4d1a535f42443d8140d29
Nick Budak <thatbudakguy@gmail.com>
2019-04-16 11:07:59 -0400

    port dict_item from ppa & use for aria-describedby

---
+++ mep/common/templatetags/mep_tags.py
@@ -0,0 +1,10 @@
+from django.template.defaulttags import register
+
+@register.filter
+def dict_item(dictionary, key):
+    ''''Template filter to allow accessing dictionary value by variable key.
+    Example use::
+
+        {{ mydict|dict_item:keyvar }}
+    '''
+    return dictionary.get(key, None)
