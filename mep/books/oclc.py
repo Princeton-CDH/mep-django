4fd0125a54dc93e74411ff8b3bb89c06df3ad2d6
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-04-16 16:45:45 -0400

    Build out SRUResponse and SRWResponse classes and tests

--- mep/books/oclc.py
+++ mep/books/oclc.py
@@ -2,14 +2,16 @@
 Module for collecting OCLC search functionality, particularly SRU style
 searches.
 '''
-from io import BytesIO
+from io import BytesIO, StringIO
 from logging import getLogger
+import time
 
 from django.conf import settings
 from django.core.exceptions import ImproperlyConfigured
 from eulxml import xmlmap
 import pymarc
 import requests
+
 
 logger = getLogger(__name__)
 
@@ -29,6 +31,7 @@ class WorldCatClientBase:
                                        'requires a WSKEY.')
         # Configure a session to use WS_KEY
         self.session = requests.Session()
+        # should we set a contact header?
         self.session.params.update({
             'wskey': wskey
         })
@@ -40,53 +43,100 @@ class WorldCatClientBase:
         """Return an AND formatting query string."""
         return ' AND '.join(self.query)
 
-
     def search(self, **kwargs):
         '''Extendable method to process the results of a search query'''
         kwargs.update({'query': self.query_string})
+        start = time.time()
         try:
             response = self.session.get(
                 ('%s/%s' % (self.WORLDCAT_API_BASE,
                             self.API_ENDPOINT)).strip('/'),
                 params=kwargs)
-            if response.status_code == requests.codes.OK:
+            # log the url call and response time
+            logger.debug('search %s => %d: %f sec',
+                         self.query_string, response.status_code,
+                         time.time() - start)
+            if response.status_code == requests.codes.ok:
                 return response
         except requests.exceptions.ConnectionError as err:
             logger.exception(err)
 
 
-class SrwResponse(xmlmap.XmlObject):
+class SRWResponse(xmlmap.XmlObject):
     '''Object to parse SRW formatted responses.'''
     ROOT_NAMESPACES = {
         'srw': 'http://www.loc.gov/zing/srw/'
     }
 
+    #: number of records found for the query
     num_records = xmlmap.IntegerField('srw:numberOfRecords')
+    #: returned records as :class:`~eulxml.xmlmap.XmlObject`
+    records = xmlmap.NodeField('srw:records', xmlmap.XmlObject)
+
+    @property
+    def marc_records(self):
+        # serialize xml to a bytebytestream for loading by pymarc
+        bytestream = BytesIO()
+        self.serializeDocument(bytestream)
+        bytestream.seek(0)
+        # pymarc is returning 'None' in the array alternating with
+        # pymarc records; not sure why, but filter them out
+        return list(filter(None, pymarc.parse_xml_to_array(bytestream)))
 
 
-class SruSearch(WorldCatClientBase):
+class SRUSearch(WorldCatClientBase):
     '''Client for peforming an SRU (specific edition) based search'''
 
     API_ENDPOINT = 'search/worldcat/sru'
 
     def __init__(self):
         super().__init__()
-        self.num_records = 0
 
-    def filter(self, filter_string):
+    # mapping of field lookup to srw field abbreviation
+    search_fields = {
+        'title': 'ti',
+        'author': 'au',
+        'year': 'yr',
+        'keyword': 'kw'
+    }
+
+    def filter(self, *args, **kwargs):
         '''Add another filter to the :class:`SRUSearch` object and return
         self to allow chaining.'''
-        self.query.append(filter_string)
-        return self
+        search_copy = self._clone()
+        search_copy.query.extend(args)
+        for key, val in kwargs.items():
+            # split field on __ to allow for specifying operator
+            field_parts = key.split('__', 1)
+            field = field_parts[0]
+
+            # convert readable field names to srw field via lookup
+            if field in self.search_fields:
+                field = 'srw.%s' % self.search_fields[field]
+
+            if len(field_parts) > 1:
+                # spaces needed for everything besides = (?)
+                operator = ' %s ' % field_parts[1]
+            else:
+                # assume equal if not specified
+                operator = '='
+
+            # *maybe* could infer when to wrap quotes around val
+            # based on type (e.g. str vs numeric)
+
+            search_copy.query.append('%s%s%s' % (field, operator, val))
+
+        return search_copy
+
+    def _clone(self):
+        search_copy = self.__class__()
+        search_copy.query = list(self.query)
+        return search_copy
 
     def search(self, **kwargs):
         '''Perform a CQL based search based on chained filters.'''
         response = super().search(**kwargs)
         if response:
-            # set the number of records for use in pulling max count
-            srw_response = xmlmap\
-                .load_xmlobject_from_string(response.text, SrwResponse)
-            if srw_response.is_valid():
-                self.num_records = srw_response.num_records
-        return pymarc.parse_xml_to_array(BytesIO(response.content))
-
+            # parse and return as SRW Response
+            return xmlmap.load_xmlobject_from_string(
+                response.content, SRWResponse)
--- mep/books/tests/test_oclc.py
+++ mep/books/tests/test_oclc.py
@@ -2,54 +2,57 @@ import os
 from unittest.mock import Mock, patch
 
 from django.core.exceptions import ImproperlyConfigured
-from django.test import SimpleTestCase
 from django.test.utils import override_settings
+from eulxml import xmlmap
+import pymarc
 import pytest
 import requests
 
-from mep.books.oclc import WorldCatClientBase
+from mep.books.oclc import WorldCatClientBase, SRUSearch, SRWResponse
 
 FIXTURES_DIR = os.path.join('mep', 'books', 'fixtures')
 
-class TestWorldCatClientBase(SimpleTestCase):
+
+class TestWorldCatClientBase:
 
     def test_init(self):
-        with self.settings(OCLC_WSKEY=''):
+        with override_settings(OCLC_WSKEY=''):
             with pytest.raises(ImproperlyConfigured):
-                wc = WorldCatClientBase()
-        with self.settings(OCLC_WSKEY='secretkey'):
-            wc = WorldCatClientBase()
-            assert isinstance(wc.session, requests.Session)
-            assert wc.session.params['wskey'] == 'secretkey'
-            assert wc.query == []
+                WorldCatClientBase()
+
+        with override_settings(OCLC_WSKEY='secretkey'):
+            wcb = WorldCatClientBase()
+            assert isinstance(wcb.session, requests.Session)
+            assert wcb.session.params['wskey'] == 'secretkey'
+            assert wcb.query == []
 
     @override_settings(OCLC_WSKEY='fakekey')
     def test_query_string(self):
-        wc = WorldCatClientBase()
-        wc.query = [
+        wcb = WorldCatClientBase()
+        wcb.query = [
             'A',
             'B',
             'C'
         ]
-        assert wc.query_string == 'A AND B AND C'
+        assert wcb.query_string == 'A AND B AND C'
 
     @patch('mep.books.oclc.requests')
     @override_settings(OCLC_WSKEY='fakekey')
     def test_search(self, mock_requests):
         # stub back in codes and exceptions for requests
-        mock_requests.codes.OK = requests.codes.OK
+        mock_requests.codes.ok = requests.codes.ok
         mock_requests.exceptions.ConnectionError = requests.exceptions.ConnectionError
         # mock out an object that can have a return_code to check
         mock_session = mock_requests.Session.return_value
         mock_session.get.return_value = Mock()
         mock_session.get.return_value.status_code = 200
-        wc = WorldCatClientBase()
-        wc.query = ['foo', 'bar']
+        wcb = WorldCatClientBase()
+        wcb.query = ['foo', 'bar']
         # successful run, status code 200
-        response = wc.search(baz='Y',bar='foo')
+        response = wcb.search(baz='Y', bar='foo')
         # calls get as specified, with passed in kwargs
         mock_session.get.assert_called_with(
-            wc.WORLDCAT_API_BASE,
+            wcb.WORLDCAT_API_BASE,
             params={
                 'query': 'foo AND bar',
                 'baz': 'Y',
@@ -60,12 +63,80 @@ class TestWorldCatClientBase(SimpleTestCase):
         assert response == mock_session.get.return_value
         # unsuccessful run, non-200 status code, returns None
         mock_session.get.return_value.status_code = 401
-        response = wc.search()
-        assert response is None
+        response = wcb.search()
+        assert not response
+
         # unsuccessful run, raises a requests exception
         # should be handled by returning None
         # make sure that the status_code doesn't affect this
         mock_session.get.return_value.status_code = 200
         mock_session.get.side_effect = requests.exceptions.ConnectionError
-        response = wc.search()
-        assert response is None
+        response = wcb.search()
+        assert not response
+
+
+class TestSRUSearch:
+    response_fixture = os.path.join(FIXTURES_DIR, 'oclc_srw_response.xml')
+
+    def test_clone(self):
+        srus = SRUSearch()
+        srus.query = ['foo']
+        clone_srus = srus._clone()
+        # query lists should be equal but *not* the same object
+        assert clone_srus.query == srus.query
+        assert clone_srus.query is not srus.query
+
+    def test_filter(self):
+        srus = SRUSearch()
+        # list of filters as args
+        filter_args = ['srw.yr=1901', 'srw.ti exact "Ulysses"']
+        filter_srus = srus.filter(*filter_args)
+        # args added to list of queries as-is
+        assert filter_srus.query == filter_args
+        # original query untouched
+        assert not srus.query
+
+        # field=value filter with lookup
+        title_srus = srus.filter(title="Ulysses")
+        # title converted to srw.ti, equal inferred
+        assert 'srw.ti=Ulysses' in title_srus.query
+        # field__operator=value filter with operator specified
+        author_srus = srus.filter(author__any="James Joyce")
+        assert 'srw.au any James Joyce' in author_srus.query
+
+    @patch('mep.books.oclc.WorldCatClientBase.search')
+    def test_search(self, mock_base_search):
+        with open(self.response_fixture) as response:
+            # return fixture as response
+            mock_base_search.return_value.content = response.read()
+            resp = SRUSearch().search()
+            # returns response object
+            assert isinstance(resp, SRWResponse)
+
+            # no response - no error, no response
+            mock_base_search.return_value = None
+            assert not SRUSearch().search()
+
+
+class TestSRWResponse:
+
+    fixture = os.path.join(FIXTURES_DIR, 'oclc_srw_response.xml')
+
+    def get_fixture_obj(self):
+        # initialize and return SRWResponse from fixture
+        return xmlmap.load_xmlobject_from_file(
+            self.fixture, SRWResponse)
+
+    def test_fields(self):
+        srw_response = self.get_fixture_obj()
+        assert srw_response.num_records == 34154
+
+    def test_marc_records(self):
+        srw_response = self.get_fixture_obj()
+        marc_records = srw_response.marc_records
+        assert len(marc_records) == 10
+        assert isinstance(marc_records[0], pymarc.record.Record)
+
+        # sanity check value from first and last record
+        marc_records[0]['001'] == 498910170
+        marc_records[-1]['001'] == 911727061
