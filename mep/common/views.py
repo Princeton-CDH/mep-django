2b322f757e632bc1a0270594f09541abf81c4200
Nick Budak <thatbudakguy@gmail.com>
2019-04-18 12:40:02 -0400

    port VaryOnHeadersMixin and AjaxTemplateMixin from ppa

--- mep/common/views.py
+++ mep/common/views.py
@@ -1,5 +1,7 @@
-from django.views.generic.base import View, ContextMixin
 from django.core.paginator import Paginator
+from django.utils.cache import patch_vary_headers
+from django.views.generic.base import ContextMixin, TemplateResponseMixin, View
+
 
 class LabeledPagesMixin(ContextMixin):
     '''View mixin to add labels for pages to a paginated view's context,
@@ -27,3 +29,38 @@ class LabeledPagesMixin(ContextMixin):
         paginator = context['page_obj'].paginator
         context['page_labels'] = self.get_page_labels(paginator)
         return context
+
+
+class VaryOnHeadersMixin(View):
+    '''View mixin to set Vary header - class-based view equivalent to
+    :meth:`django.views.decorators.vary.vary_on_headers`, adapted from
+    winthrop-django.
+
+    Define :attr:`vary_headers` with the list of headers.
+    '''
+
+    vary_headers = []
+
+    def dispatch(self, request, *args, **kwargs):
+        '''Wrap default dispatch method to patch haeders on the response.'''
+        response = super(VaryOnHeadersMixin, self).dispatch(request, *args, **kwargs)
+        patch_vary_headers(response, self.vary_headers)
+        return response
+
+
+class AjaxTemplateMixin(TemplateResponseMixin, VaryOnHeadersMixin):
+    '''View mixin to use a different template when responding to an ajax
+    request.'''
+
+    #: name of the template to use for ajax request
+    ajax_template_name = None
+    #: vary on X-Request-With to avoid browsers caching and displaying
+    #: ajax response for the non-ajax response
+    vary_headers = ['X-Requested-With']
+
+    def get_template_names(self):
+        '''Return :attr:`ajax_template_name` if this is an ajax request;
+        otherwise return default template name.'''
+        if self.request.is_ajax():
+            return self.ajax_template_name
+        return super().get_template_names()
