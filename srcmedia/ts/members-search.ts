5ae4cfe82572ff52e5570fbf7074e8570ff9f56e
Nick Budak <thatbudakguy@gmail.com>
2019-04-29 14:12:35 -0400

    add logic for updating page labels

--- srcmedia/ts/members-search.ts
+++ srcmedia/ts/members-search.ts
@@ -1,6 +1,7 @@
-import { merge, partition, Observable } from 'rxjs'
-import { pluck, map, withLatestFrom, startWith } from 'rxjs/operators'
+import { merge, partition } from 'rxjs'
+import { pluck, map, withLatestFrom, startWith, distinctUntilChanged } from 'rxjs/operators'
 
+import { arraysAreEqual } from './lib/common'
 import { RxTextInput, RxCheckboxInput } from './lib/input'
 import { RxOutput } from './lib/output'
 import { RxSearchForm } from './lib/form'
@@ -37,49 +38,63 @@ document.addEventListener('DOMContentLoaded', () => {
     // fulfill the condition and those that don't
     const [keywordOn, keywordOff] = partition(
         keywordInput.state.pipe(pluck('value')), value => value === '')
-    keywordOn.subscribe(() => sortSelect.update({ value: 'name' }))
-    keywordOff.subscribe(() => sortSelect.update({ value: 'relevance' }))
+    keywordOn.subscribe(() => sortSelect.value.next('name'))
+    keywordOff.subscribe(() => sortSelect.value.next('relevance'))
 
     // When a user changes the search or sort, go back to page 1
     merge(
         keywordInput.state,
         hasCardInput.state,
-        sortSelect.state,
+        sortSelect.value,
     ).subscribe(() => {
-        pageSelect.update({ value: '1' })
+        pageSelect.value.next('1')
+        totalResultsOutput.update('Results are loading')
     })
 
     // When the page is changed, submit the form and apply loading styles
-    pageSelect.state.subscribe(() => {
+    pageSelect.value.subscribe(() => {
         membersSearchForm.submit()
         resultsOutput.element.setAttribute('aria-busy', '') // empty string used for boolean attributes
-        totalResultsOutput.update('Results are loading')
     })
 
     // When next/previous page links are clicked, go to that page
     pageControls.pageChanges.pipe( // for every next/prev click
-        withLatestFrom(pageSelect.state.pipe( // get the current page
-            pluck('value'), // which is the value of the <select>
-            startWith($pageSelect.value), // start with the one it's currently on
+        withLatestFrom(pageSelect.value.pipe( // get the current page
+            startWith(pageSelect.element.value), // start with the one it's currently on
             map(pageNumber => parseInt(pageNumber)) // parse into an integer
-        ))
-    ).pipe( // withLatestFrom will return an array of the two observables we merged
+        )),
+    // ).pipe( // withLatestFrom will return an array of the two observables we merged
         map(([action, currentPage]) => { // if next, add 1, otherwise subtract 1
             return (action === 'next' ? currentPage + 1 : currentPage - 1)
         }), // create a { value: nextPage } object to pass back to the <select>
-        map(nextPageNumber => ({ value: nextPageNumber.toString() }))
-    ).subscribe(pageSelect.update) // update the <select> as though we chose that page
+        map(nextPageNumber => nextPageNumber.toString())
+    ).subscribe(pageSelect.value) // update the <select> as though we chose that page
     
     // When the page changes, update the page controls in case next/prev become disabled
-    pageSelect.state.pipe(
-        pluck('value'),
-        startWith($pageSelect.value),
+    pageSelect.value.pipe(
+        startWith(pageSelect.element.value),
         map(pageNumber => parseInt(pageNumber)), // get the current page number, as above
-        map(pageNumber => [pageNumber, $pageSelect.length]) // append the total number of pages
+        map(pageNumber => [pageNumber, pageSelect.element.length]) // append the total number of pages
     ).subscribe(pageControls.update)
+
+    // When the page labels change, pass them to the page select to re-render options
+    membersSearchForm.pageLabels.pipe(
+        distinctUntilChanged(arraysAreEqual), // don't do anything if the page labels didn't change
+        withLatestFrom(pageSelect.value.pipe( // get the current page number
+            map(pageNumber => parseInt(pageNumber))
+        )),
+        map(([labels, currentPage]) => {
+            return labels.map((label, pageNumber) => ({
+                value: (pageNumber + 1).toString(), // <option> value is its index (page number)
+                text: label, // text is the page label
+                selected: (pageNumber + 1 === currentPage) // selected if it's the page we're on
+            }))
+        })
+    ).subscribe(pageSelect.options)
 
     // When the number of total results changes, display it
     membersSearchForm.totalResults.pipe(
+        distinctUntilChanged(),
         map(total => `${total} total results`)
     ).subscribe(totalResultsOutput.update)
 
