6856ba8993db073af61267268c5c0aa47b8e87aa
Nick Budak <thatbudakguy@gmail.com>
2019-05-06 15:10:55 -0400

    add convenience SCHEMA_ORG namespace

--- mep/common/__init__.py
+++ mep/common/__init__.py
@@ -4,8 +4,10 @@ across other applications within the project.  Currently includes
 abstract database models with common functionality or fields that are
 used by models in multiple apps within the project.
 '''
+import rdflib
 
 default_app_config = 'mep.common.apps.CommonConfig'
+SCHEMA_ORG = rdflib.Namespace('http://schema.org/')
 
 
 from mep.common.solr import SolrSchema
