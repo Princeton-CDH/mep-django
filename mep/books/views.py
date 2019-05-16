8b49dccd23d1ef8a27ab3ee67136e3cded63d810
Nick Budak <thatbudakguy@gmail.com>
2019-05-16 12:58:02 -0400

    wip

--- mep/books/views.py
+++ mep/books/views.py
@@ -1,6 +1,6 @@
 from dal import autocomplete
 from django.db.models import Q
-from django.views.generic import ListView
+from django.views.generic import DetailView, ListView
 
 from mep.books.models import Item
 from mep.books.queryset import ItemSolrQuerySet
@@ -16,6 +16,13 @@ class ItemList(LabeledPagesMixin, ListView):
 
     def get_queryset(self):
         return ItemSolrQuerySet().order_by('title')
+
+
+class ItemDetail(DetailView):
+    '''Detail page for a single library book.'''
+    model = Item
+    template_name = 'books/item_detail.html'
+    context_object_name = 'item'
 
 
 class ItemAutocomplete(autocomplete.Select2QuerySetView):
