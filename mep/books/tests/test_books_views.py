4044d128f18295b7b1337b9f0be63046d25c07b2
Nick Budak <thatbudakguy@gmail.com>
2019-06-26 11:13:12 -0400

    add ItemList tests for get_queryset and get_page_labels

--- mep/books/tests/test_books_views.py
+++ mep/books/tests/test_books_views.py
@@ -1,10 +1,13 @@
 import time
+from unittest.mock import Mock
 
 from django.contrib.auth import get_user_model
+from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
-from django.test import TestCase
+from django.test import RequestFactory, TestCase
 
 from mep.books.models import Item
+from mep.books.views import ItemList
 
 
 class TestBooksViews(TestCase):
@@ -61,13 +64,15 @@ class TestItemListView(TestCase):
     fixtures = ['sample_items.json', 'multi_creator_item.json']
 
     def setUp(self):
+        # index fixtures and give time for index to take effect
         Item.index_items(Item.objects.all())
-        # give time for index to take effect
         time.sleep(10)
+        # bind some convenience items
+        self.factory = RequestFactory()
+        self.url = reverse('books:books-list')
 
     def test_list(self):
-        url = reverse('books:books-list')
-        response = self.client.get(url)
+        response = self.client.get(self.url)
 
         # should display all items in the database
         items = Item.objects.all()
@@ -82,12 +87,10 @@ class TestItemListView(TestCase):
         # NOTE publishers display is designed but data not yet available
 
     def test_many_authors(self):
-        url = reverse('books:books-list')
-        response = self.client.get(url)
-
-        novelists = Item.objects.get(pk=4126)
+        response = self.client.get(self.url)
 
         # multi-author item should show first three authors
+        novelists = Item.objects.get(pk=4126)
         self.assertContains(response, novelists.authors.first())
         self.assertContains(response, novelists.authors[1])
         self.assertContains(response, novelists.authors[2])
@@ -97,3 +100,42 @@ class TestItemListView(TestCase):
 
         # should show "...x more authors" text
         self.assertContains(response, '... 16 more authors')
+
+    def test_get_queryset(self):
+        # create a mocked form
+        view = ItemList()
+        form = Mock()
+        view.get_form = Mock(return_value=form)
+        view.request = self.factory.get(self.url)
+        # if form is valid, should return all items sorted by chosen sort
+        form.is_valid.return_value = True
+        form.cleaned_data = {'sort': 'title'}  # becomes 'title_s'
+        solr_qs = view.get_queryset()
+        db_qs = Item.objects.order_by('title')
+        # querysets from solr and db should match
+        for index, item in enumerate(solr_qs):
+            assert db_qs[index].title == item['title']
+            assert db_qs[index].pk == item['pk']
+        # if form is invalid, should return empty queryset
+        form.is_valid.return_value = False
+        solr_qs = view.get_queryset()
+        # NOTE replace with EmptySolrQueryset check when implemented in parasolr
+        assert solr_qs.count() == 0
+
+    def test_get_page_labels(self):
+        # create a mocked form and some fake items to paginate
+        view = ItemList()
+        form = Mock()
+        view.get_form = Mock(return_value=form)
+        view.request = self.factory.get(self.url)
+        items = range(120)
+        paginator = Paginator(items, per_page=view.paginate_by)
+        # if form is valid, should use default implementation (numbered pages)
+        # NOTE this will change if we implement alpha pagination for this view
+        form.is_valid.return_value = True
+        page_labels = view.get_page_labels(paginator)
+        assert page_labels == [(1, '1 - 100'), (2, '101 - 120')]
+        # if invalid, should return one page with 'N/A' label
+        form.is_valid.return_value = False
+        page_labels = view.get_page_labels(paginator)
+        assert page_labels == [(1, 'N/A')]
