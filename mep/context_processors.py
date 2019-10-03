316f49b7c937dd977dda2471f4ed372aaddaad68
Nick Budak <thatbudakguy@gmail.com>
2019-10-03 11:45:06 -0400

    use None for some context_extras

--- mep/context_processors.py
+++ mep/context_processors.py
@@ -6,7 +6,7 @@ def template_settings(request):
 
     context_extras = {
         'SHOW_TEST_WARNING': getattr(settings, 'SHOW_TEST_WARNING', False),
-        'GTAGS_ANALYTICS_ID': getattr(settings, 'GTAGS_ANALYTICS_ID', False),
-        'GTAGS_ANALYTICS_ENV': getattr(settings, 'GTAGS_ANALYTICS_ENV', False)
+        'GTAGS_ANALYTICS_ID': getattr(settings, 'GTAGS_ANALYTICS_ID', None),
+        'GTAGS_ANALYTICS_ENV': getattr(settings, 'GTAGS_ANALYTICS_ENV', None)
     }
     return context_extras
