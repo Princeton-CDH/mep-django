d72630b869abbab86121bcacb5792c084973fc74
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-10-08 09:37:12 -0400

    Index logbook and card year/month activity for members

--- mep/people/queryset.py
+++ mep/people/queryset.py
@@ -18,6 +18,8 @@ class PersonSolrQuerySet(AliasedSolrQuerySet):
         'account_end': 'account_end_i',
         'account_years': 'account_years_is',
         'account_yearmonths': 'account_yearmonths_is',
+        'logbook_yearmonths': 'logbook_yearmonths_is',
+        'card_yearmonths': 'card_yearmonths_is',
         'has_card': 'has_card_b',
         'pk': 'pk_i',
         'sex': 'sex_s',
