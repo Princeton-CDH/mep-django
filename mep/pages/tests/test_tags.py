f3245905506ac4e723f89bdb3e37f9ae9adda4ce
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-10-10 10:12:18 -0400

    Add logic for software version & date in citation; adapted from PPA

---
+++ mep/pages/tests/test_tags.py
@@ -0,0 +1,15 @@
+from django.utils.timezone import now
+
+from mep.pages.templatetags.mep_page_tags import format_citation
+
+
+def test_format_citation():
+    # fields replaced when present
+    citation = 'S&co, version [SW_VERSION]. Accessed [DATE].'
+    fmt_citation = format_citation(citation, '3.0')
+    assert 'version 3.0.' in fmt_citation
+    assert 'Accessed %s' % now().strftime('%d %B %Y') in fmt_citation
+
+    # nothing changed if not present
+    text = 'some other paragraph text with <i>formatting</i> etc'
+    assert format_citation(text, '3.0') == text
