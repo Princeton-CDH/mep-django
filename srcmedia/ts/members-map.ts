3f596fd084b52b35425d249390cc2a00c19423bb
Nick Budak <thatbudakguy@gmail.com>
2019-10-24 15:55:39 -0400

    working leaflet map

--- srcmedia/ts/members-map.ts
+++ srcmedia/ts/members-map.ts
@@ -1,4 +1,15 @@
 import { map } from 'leaflet'
+import { TiledMapLayer } from 'esri-leaflet'
 
 const target = document.getElementById('address-map') as HTMLDivElement
 const addressMap = map(target)
+
+const parisTiles = new TiledMapLayer({
+    url: 'https://tiles.arcgis.com/tiles/4Ko8f1mCWFLyY4NV/arcgis/rest/services/Paris_1943/MapServer',
+    attribution: 'Map <a href="https://maps.princeton.edu/catalog/princeton-2r36tz994">Princeton University Library</a>',
+    maxZoom: 19,
+})
+
+parisTiles.addTo(addressMap)
+
+addressMap.setView([48.8589101, 2.3120407], 14)
