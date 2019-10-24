05e5446aff81b43f06af99ce32b8c231a117b747
Nick Budak <thatbudakguy@gmail.com>
2019-10-24 16:12:13 -0400

    adjust zoom level and bounds of map

--- srcmedia/ts/members-map.ts
+++ srcmedia/ts/members-map.ts
@@ -1,4 +1,4 @@
-import { map } from 'leaflet'
+import { map, latLng, latLngBounds } from 'leaflet'
 import { TiledMapLayer } from 'esri-leaflet'
 
 const target = document.getElementById('address-map') as HTMLDivElement
@@ -7,9 +7,15 @@ const addressMap = map(target)
 const parisTiles = new TiledMapLayer({
     url: 'https://tiles.arcgis.com/tiles/4Ko8f1mCWFLyY4NV/arcgis/rest/services/Paris_1943/MapServer',
     attribution: 'Map <a href="https://maps.princeton.edu/catalog/princeton-2r36tz994">Princeton University Library</a>',
-    maxZoom: 19,
+    maxZoom: 18,
+    minZoom: 13,
 })
 
 parisTiles.addTo(addressMap)
 
 addressMap.setView([48.8589101, 2.3120407], 14)
+
+addressMap.setMaxBounds(latLngBounds(
+    latLng(48.906619, 2.243891),
+    latLng(48.810564, 2.422762)
+))
