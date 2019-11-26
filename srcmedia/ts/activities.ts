fc6bf45e86368a68c07cfa43c95b72fa99901642
Nick Budak <thatbudakguy@gmail.com>
2019-11-26 16:47:46 -0500

    finish mobile & desktop styling for activity tables

--- srcmedia/ts/activities.ts
+++ srcmedia/ts/activities.ts
@@ -11,28 +11,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /* ELEMENTS */
     const $table = document.getElementById('activity-table') as HTMLElement
-    const $form = document.getElementById('activity-mobile-control') as HTMLElement
-    const $expand = document.getElementById('expand') as HTMLFormElement
-    const $events = document.getElementsByTagName('tr') as HTMLCollection
-
-    const $tableHeader = document.getElementsByTagName('thead')[0] as HTMLElement
-    const $tableHeaderElement = document.getElementsByTagName('th')[0] as HTMLElement
+    const $tableHeader = document.getElementsByTagName('th')[0] as HTMLElement
 
     /* COMPONENTS */
+
     // bind to header element but set stuck class on the table header
     // const stickyControls = new StickyControls($tableHeaderElement, $tableHeader)
-    const stickyControls = new StickyControls($tableHeaderElement)
-
-    Tablesort($table)
+    new StickyControls($tableHeader)
 
     /* BINDINGS */
+
+    // make table sortable
+    Tablesort($table)
+
     // on tablet only, we need to manually set row heights so that the leftmost
     // table column can be sticky
+    /*
     if (window.matchMedia('(min-width: 768px) and (max-width: 1023px)').matches) {
-        const firsts = document.querySelectorAll('td.title') as NodeListOf<HTMLTableCellElement>
+        const firsts = document.querySelectorAll('td:first-child') as NodeListOf<HTMLTableCellElement>
         firsts.forEach(e => {
             const start = e.nextElementSibling as HTMLElement
             setAllHeights(start, getComputedStyle(e).getPropertyValue('height'))
         })
-    }
+    } */
 })
