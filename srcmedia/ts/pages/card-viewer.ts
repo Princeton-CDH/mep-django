10ed9ab3a2780283bc506a769c17db6836992415
Nick Budak <nbudak@princeton.edu>
2020-02-20 10:11:30 -0500

    auto-scroll active card into view

--- srcmedia/ts/pages/card-viewer.ts
+++ srcmedia/ts/pages/card-viewer.ts
@@ -4,10 +4,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
     /* ELEMENTS */
     const $tableHeader = document.getElementsByTagName('th')[0] as HTMLElement
+    const $activeCard = document.querySelector('.card.active') as HTMLLIElement
+    const $cardNav = document.querySelector('nav.cards') as HTMLElement
 
     /* COMPONENTS */
 
     // bind to header element but set stuck class on the table header
     // const stickyControls = new StickyControls($tableHeaderElement, $tableHeader)
-    new StickyControls($tableHeader)
+    if ($tableHeader) new StickyControls($tableHeader)
+
+    /* BINDINGS */
+    scrollToActiveCard($cardNav, $activeCard)
 })
+
+/**
+ * Automatically scroll the card navigation to focus the currently viewed card.
+ * 
+ * @param nav 
+ * @param activeCard 
+ */
+function scrollToActiveCard(nav: HTMLElement, activeCard: HTMLLIElement): void {
+    // distance we want to scroll is (offset - card width - margin)px
+    const style = getComputedStyle(activeCard)
+    const margin = style.marginRight ? parseInt(style.marginRight.split('px')[0]) : 0
+    nav.scroll(activeCard.offsetLeft - activeCard.offsetWidth - margin, 0)
+}
