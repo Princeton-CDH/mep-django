1e346f27fd99526b012f57a4b75fb9b48c20ecb2
Nick Budak <nbudak@princeton.edu>
2020-04-07 15:47:23 -0400

    change uncertainty message comment

--- mep/books/models.py
+++ mep/books/models.py
@@ -224,10 +224,8 @@ class Work(Notable, ModelIndexable):
     '''Work record for an item that circulated in the library or was
     other referenced in library activities.'''
 
-    '''
-    Message that will be read to users of assistive technology in place of
-    the uncertainty icon.
-    '''
+    #: Message that will be read to users of assistive technology in place of
+    #: the uncertainty icon.
     UNCERTAINTY_MESSAGE = "Work data is uncertain or incomplete."
 
     #: mep id from stub records imported from xml
