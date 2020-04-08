ec71f80b942d0a730f6df07708203ee470d9f1a1
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-04-08 16:55:07 -0400

    Add all book sort options to book search form

--- mep/books/forms.py
+++ mep/books/forms.py
@@ -7,18 +7,26 @@ class WorkSearchForm(FacetForm):
     '''Book search form'''
 
     SORT_CHOICES = [
+        ('title', 'Title A – Z'),
+        ('title_za', 'Title Z – A'),
+        ('author', 'Author A – Z'),
+        ('author_za', 'Author Z – A'),
+        ('borrowing_desc', 'Borrowing Frequency Highest – Lowest'),
+        ('borrowing', 'Borrowing Frequency Lowest – Highest'),
+        ('pubdate', 'Publication Date Oldest – Newest'),
+        ('pubdate_desc', 'Publication Date Newest – Oldest'),
         ('relevance', 'Relevance'),
-        ('title', 'Title A - Z'),
     ]
 
     # NOTE these are not set by default!
     error_css_class = 'error'
     required_css_class = 'required'
 
-    query = forms.CharField(label='Keyword or Phrase', required=False,
-                        widget=forms.TextInput(attrs={
-                            'placeholder': 'Search book',
-                            'aria-label': 'Keyword or Phrase'
-                        }))
+    query = forms.CharField(
+        label='Keyword or Phrase', required=False,
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Search book',
+            'aria-label': 'Keyword or Phrase'
+        }))
     sort = forms.ChoiceField(choices=SORT_CHOICES, required=False,
                              widget=SelectWithDisabled)
