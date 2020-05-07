925e77198a68ff74eb9216248f04043bd506db44
Nick Budak <nbudak@princeton.edu>
2020-05-07 14:42:59 -0400

    rework ActiveFilters to use associated form instead of querystring

--- srcmedia/ts/components/ActiveFilters.test.ts
+++ srcmedia/ts/components/ActiveFilters.test.ts
@@ -2,22 +2,30 @@ import ActiveFilters from './ActiveFilters'
 
 describe('ActiveFilters component', () => {
 
-    beforeEach(() => {
-        // make the DOM reflect the currently active filters
+    it('finds the parent form on initialization', () => {
+        // set up the DOM with no active filters
         document.body.innerHTML = `
-        <div class='active-filters'>
-            <span class='legend'>Selected Filters</span>
-            <a href='http://example.com/'>United States</a>
-            <a href='http://example.com/'>France</a>
-            <a href='http://example.com/'>Female</a>
-            <a href='http://example.com/'>Has Card</a>
-            <a href='http://example.com/'>Member 1920 – 1940</a>
-            <a href='http://example.com/' class='clear-all'>Clear All</a>
-        </div>
-        `
+        <form id="test-form">
+        <div class="active-filters">
+            <span class="legend hidden">Selected Filters</span>
+            <a href="http://example.com/" class="clear-all hidden">Clear All</a>
+        </div></form>`
+        const $target = document.querySelector('.active-filters') as HTMLDivElement
+        const af = new ActiveFilters($target)
+
+        // keeps reference to parent form
+        const $form = document.querySelector('form') as HTMLFormElement
+        expect(af.$form).toBe($form)
     })
 
     it('finds the legend element on initialization', () => {
+        // set up the DOM with no active filters
+        document.body.innerHTML = `
+        <form id="test-form">
+        <div class="active-filters">
+            <span class="legend hidden">Selected Filters</span>
+            <a href="http://example.com/" class="clear-all hidden">Clear All</a>
+        </div></form>`
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
@@ -27,6 +35,13 @@ describe('ActiveFilters component', () => {
     })
 
     it('finds the "clear all" button on initialization', () => {
+        // set up the DOM with no active filters
+        document.body.innerHTML = `
+        <form id="test-form">
+        <div class="active-filters">
+            <span class="legend hidden">Selected Filters</span>
+            <a href="http://example.com/" class="clear-all hidden">Clear All</a>
+        </div></form>`
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
@@ -36,10 +51,23 @@ describe('ActiveFilters component', () => {
     })
 
     it('converts all links into buttons on initialization', () => {
+        // set up the DOM with some active filters
+        document.body.innerHTML = `
+        <form id="test-form">
+        <div class='active-filters'>
+            <span class='legend'>Selected Filters</span>
+            <a href='http://example.com/'>United States</a>
+            <a href='http://example.com/'>France</a>
+            <a href='http://example.com/'>Female</a>
+            <a href='http://example.com/'>Has Card</a>
+            <a href='http://example.com/'>Member 1920 – 1940</a>
+            <a href='http://example.com/' class='clear-all'>Clear All</a>
+        </div></form>
+        `
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
-        // hrefs should be stripped, should get role='button'
+        // all a hrefs should be stripped, should get role='button'
         const $anchors = document.querySelectorAll('a')
         $anchors.forEach($anchor => {
             expect($anchor.getAttribute('role')).toBe('button')
@@ -47,46 +75,63 @@ describe('ActiveFilters component', () => {
         })
     })
 
-    it('reads boolean filter from querystring on initialization', () => {
-        // set up page with boolean filter active
-        history.replaceState({}, 'test', 'http://localhost/?is_cool=on')
+    it.skip('reads boolean filter from form on initialization', () => {
+        // set up page with a boolean filter active
+        document.body.innerHTML = `
+        <form id="test-form">
+        <input type="checkbox" name="foo" checked="">
+        <div class="active-filters">
+            <span class="legend">Selected Filters</span>
+            <a href="http://example.com/">Is Cool</a>
+            <a href="http://example.com/" class="clear-all">Clear All</a>
+        </div>
+        </form>`
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
         // boolean should be stored; value doesn't matter so we use it unchanged
-        const watcher = jest.fn()
-        af.filters.subscribe(watcher)
-        const initState: Map<string, any> = watcher.mock.calls[0][0]
-        expect(initState.get('is_cool')).toBe('on')
     })
 
-    it('reads multivalued filter from querystring on initialization', () => {
+    it.skip('reads multivalued filter from form on initialization', () => {
         // set up page with multivalued text facet active
-        history.replaceState({}, 'test', 'http://localhost/?foo=bar&foo=baz')
+        document.body.innerHTML = `
+        <form id="test-form">
+        <div class="active-filters">
+            <span class="legend">Selected Filters</span>
+            <a href="http://example.com/">Bar</a>
+            <a href="http://example.com/">Baz</a>
+            <a href="http://example.com/" class="clear-all">Clear All</a>
+        </div></form>`
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
         // facet values should be collapsed into an array
-        const watcher = jest.fn()
-        af.filters.subscribe(watcher)
-        const initState: Map<string, any> = watcher.mock.calls[0][0]
-        expect(initState.get('foo')).toEqual(['bar', 'baz'])
     })
 
-    it('reads range filter from querystring on initialization', () => {
+    it.skip('reads range filter from form on initialization', () => {
         // set up page with range filter active
-        history.replaceState({}, 'test', 'http://localhost/?foo_1=100&foo_2=150')
+        document.body.innerHTML = `
+        <form id="test-form">
+        <div class="active-filters">
+            <span class="legend">Selected Filters</span>
+            <a href="http://example.com/">Foo: 100 - 150</a>
+            <a href="http://example.com/" class="clear-all">Clear All</a>
+        </div></form>`
         const $target = document.querySelector('.active-filters') as HTMLDivElement
         const af = new ActiveFilters($target)
 
         // both ends of range should be under name of property ('foo')
-        const watcher = jest.fn()
-        af.filters.subscribe(watcher)
-        const initState: Map<string, any> = watcher.mock.calls[0][0]
-        expect(initState.get('foo')).toEqual(['100', '150'])
     })
 
-    it.todo('renders a clickable button for each active filter')
+    it.todo('renders a clickable button for each active filter', () => {
+        // set up the DOM with no active filters
+        document.body.innerHTML = `
+        <div class="active-filters">
+            <span class="legend hidden">Selected Filters</span>
+            <a href="http://example.com/" class="clear-all hidden">Clear All</a>
+        </div>`
+
+    })
 
     it.todo('removes an active filter button when the user clicks it')
 
--- srcmedia/ts/components/ActiveFilters.ts
+++ srcmedia/ts/components/ActiveFilters.ts
@@ -1,53 +1,118 @@
-import { Rx } from '../lib/common'
-import { Observable, of } from 'rxjs'
-import { publishBehavior } from 'rxjs/operators'
+import { Component } from '../lib/common'
+import { fakeToggle, fakeValueChange } from '../lib/common'
 
-type State = Map<String, any>
+type Filters = Map<string, any>
 
-export default class ActiveFilters extends Rx<HTMLElement> {
+export default class ActiveFilters extends Component {
+    $form: HTMLFormElement         // form whose filters we want to track
     $legend: HTMLElement           // 'active filters' text
     $clearAll: HTMLAnchorElement   // 'clear all' button
-    filters: Observable<State>     // list of currently active filters
 
     constructor(element: HTMLElement) {
         super(element)
 
-        // find the legend and clear-all buttons and store
-        this.$legend = this.element.querySelector('.legend') as HTMLElement
-        this.$clearAll = this.element.querySelector('.clear-all') as HTMLAnchorElement
+        // find the containing form and store
+        const form = this.element.closest('form')
+        if (!form) console.error(`No containing form found for ${element}`)
+        else this.$form = form
 
-        // turn all links currently inside the element into buttons
+        // find the legend and store
+        const legend = this.element.querySelector('.legend')
+        if (!legend) console.error(`No .legend element found in ${element}`)
+        else this.$legend = (legend as HTMLElement)
+
+        // find the 'clear all' button and store
+        const clearAll = this.element.querySelector('.clear-all')
+        if (!clearAll) console.error(`No .clear-all element found in ${element}`)
+        else this.$clearAll = (clearAll as HTMLAnchorElement)
+
+        // turn all links currently inside the element into buttons w/ handlers
         const $anchors = this.element.querySelectorAll('a')
         $anchors.forEach(($anchor: HTMLAnchorElement) => {
             $anchor.removeAttribute('href')
             $anchor.setAttribute('role', 'button')
+            ActiveFilters.bindClearHandler($anchor)
         })
+    }
 
-        // parse the querystring and set the initial state of activeFilters
-        const params = new URLSearchParams(window.location.search)
-        const initFilters: State = new Map()
+    /**
+     * Given an anchor element (in this case we use <a> as buttons), binds a
+     * click handler that clears the input whose id is stored in the data-input
+     * attribute of the anchor element.
+     * 
+     * Handler clears the provided input element, either by untoggling or 
+     * removing its current value. Dispatches the appropriate event as though
+     * the user had cleared it manually.
+     * 
+     * @param $button 
+     */
+    static bindClearHandler($button: HTMLAnchorElement): void {
+        const inputId = $button.dataset.input
 
-        for (let [key, val] of params.entries()) {
+        if (!inputId) {
+            console.error(`No associated input for ${$button}`)
+            return
+        }
+
+        // add event handler to toggle associated input
+        else {
+            const $input = document.getElementById(inputId) as HTMLInputElement
+
+            if (!$input) {
+                console.error(`Couldn't find element #${inputId}`)
+                return
+            }
+
+            $button.addEventListener('click', () => {
+                if ($input.type == 'checkbox') fakeToggle($input)
+                else fakeValueChange($input, '')
+            })
+        }
+    }
+
+    /**
+     * Parse the current querystring into a data structure representing active
+     * filters. Collapses ranges and multivalued fields into arrays stored at
+     * the same key.
+     */
+    public filters(): Filters {
+        const data = new FormData(this.$form)
+        const filters: Filters = new Map()
+
+        for (let [key, val] of data.entries()) {
             // collapse ranges into a single entry
             const matchRange = key.match(/^(.*)_\d+$/)
             key = matchRange ? matchRange[1] : key
-
+            
             // store multivalued facets/filters in an array
-            const current = initFilters.get(key)
-            if (!current) initFilters.set(key, val)
+            const current = filters.get(key)
+            if (!current) filters.set(key, val)
             else if (Array.isArray(current))
-                initFilters.set(key, [...current, val])
-            else initFilters.set(key, [current, val])
+                filters.set(key, [...current, val])
+            else filters.set(key, [current, val])
         }
 
-        // multicast the state and start with the initial value
-        this.filters = of(initFilters).pipe(publishBehavior(initFilters))
-
-        // re-render when state changes
-        this.filters.subscribe(this.render)
+        return filters
     }
 
-    private render(filters: State) {
-        // this.$clearAll.insertAdjacentElement('beforebegin', )
+    /**
+     * Fill the element with buttons that correspond to the provided list of
+     * filters. Clicking each button removes it and emits an event that can be
+     * used to update the state of the corresponding filter.
+     * 
+     * @param filters 
+     */
+    public render(filters: Filters) {
+        filters.forEach((val, key) => {
+            // create the button
+            const $button = document.createElement('a')
+            $button.setAttribute('role', 'button')
+            $button.innerText = key
+
+            // TODO create the event handler
+
+            // add it to the inside of the element
+            this.$clearAll.insertBefore($button, null)
+        })
     }
 }
