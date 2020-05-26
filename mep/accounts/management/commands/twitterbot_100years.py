1f519f04df0964615cc01e1496127db4432e7c63
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-05-26 11:51:07 -0400

    Only include events with fullly known start dates for 100 years twitter

--- mep/accounts/management/commands/twitterbot_100years.py
+++ mep/accounts/management/commands/twitterbot_100years.py
@@ -2,9 +2,11 @@ import datetime
 
 from dateutil.relativedelta import relativedelta
 from django.core.management.base import BaseCommand
+from django.db.models import Q
 from django.urls import reverse
 
 from mep.accounts.models import Event
+from mep.accounts.partial_date import DatePrecision
 from mep.common.utils import absolutize_url
 
 
@@ -12,6 +14,9 @@ class Command(BaseCommand):
 
     # date format:  Saturday, May 8, 1920
     date_format = '%A, %B %-d, %Y'
+
+    full_precision = DatePrecision.year | DatePrecision.month | \
+        DatePrecision.day
 
     def add_arguments(self, parser):
         parser.add_argument(
@@ -31,7 +36,9 @@ class Command(BaseCommand):
 
         # find all events for that date
         # TODO: exclude partially known dates
-        events = Event.objects.filter(start_date=date)
+        events = Event.objects.filter(start_date=date) \
+            .filter(Q(start_date_precision__isnull=True) |
+                    Q(start_date_precision=int(self.full_precision))) \
 
         print('%s %d event%s' % (date, events.count(),
                                  's' if events.count() != 1 else ''))
@@ -49,11 +56,12 @@ class Command(BaseCommand):
 
 
 def tweet_content(ev):
-    # TODO: handle multiple events for the same account on the same day
+    # TODO: handle multiple events for the same account on the same day?
     # TODO: handle multiple members
     member = ev.account.persons.first()
     prolog = '#100YearsAgoToday on %s:' % \
         ev.start_date.strftime(Command.date_format)
+    content = None
 
     if ev.event_type == 'Subscription':
         content = '%s joined the Shakespeare and Company lending library.' % \
@@ -99,15 +107,17 @@ def work_label(work):
         parts.append('an issue of')
 
     if work.authors:
-        # TODO: handle multi-author? editor?
+        # TODO: handle multiple authors
+        # two: Francis Beaumont and John Fletcher's "Plays."
+        # more than (3?): Francis Beaument et al.'s "title"'
         parts.append('%s’s' % work.authors[0].name)
 
+    # TODO: include editor if there are no authors, e.g.
+    # "Book of Gardening," edited by John Smith (DATE)
+
     # should always have title; use quotes since we can't italicize
-    if work.title.startswith('"'):
-        # don't add quotes if already present (uncertain title)
-        parts.append(work.title)
-    else:
-        parts.append('“%s”' % work.title)
+    # strip quotes if already present (uncertain title)
+    parts.append('“%s”' % work.title.strip('"“”'))
 
     # include work year if known not before 1500
     if work.year and work.year > 1500:
--- mep/accounts/views.py
+++ mep/accounts/views.py
@@ -6,6 +6,7 @@ from django.db.models import Q
 from django.views.generic import ListView
 
 from mep.accounts.models import Account, Event
+from mep.accounts.partial_date import DatePrecision
 from mep.common.views import LoginRequiredOr404Mixin
 
 
@@ -32,15 +33,17 @@ class Twitter100yearsReview(LoginRequiredOr404Mixin, ListView):
     model = Event
     template_name = 'accounts/100years_twitter_review.html'
 
+    full_precision = DatePrecision.year | DatePrecision.month | \
+        DatePrecision.day
+
     def get_queryset(self):
         date = datetime.date.today()
         # determine date 100 years earlier
         date = date - relativedelta(years=100)
-        two_weeks_ahead = date + relativedelta(weeks=2)
+        two_weeks_ahead = date + relativedelta(weeks=4)
         return super().get_queryset() \
-                      .filter(start_date__gte=date,
-                              start_date__lte=two_weeks_ahead) \
-                      .order_by('start_date', 'account')
-                      # FIXME: why does this break the query?
-                      # , start_date_precision__knownyear=True)
-
+            .filter(start_date__gte=date,
+                    start_date__lte=two_weeks_ahead) \
+            .filter(Q(start_date_precision__isnull=True) |
+                    Q(start_date_precision=int(self.full_precision))) \
+            .order_by('start_date', 'account')
