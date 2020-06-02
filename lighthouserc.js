985a56bf988628ece0058d092a32db9d92dce4b2
Nick Budak <nbudak@princeton.edu>
2020-06-01 22:03:56 -0400

    add lighthouse URLs and set readyPattern

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,8 +1,16 @@
 module.exports = {
     ci: {
         collect: {
-            url: ['http://localhost:8000/'],
-            startServerCommand: 'python manage.py runserver --insecure'
+            url: [
+                'http://localhost:8000/',
+                'http://localhost:8000/members',
+                'http://localhost:8000/books',
+                'http://localhost:8000/analysis',
+                'http://localhost:8000/sources',
+                'http://localhost:8000/about',
+            ],
+            startServerCommand: 'python manage.py runserver --insecure',
+            startServerReadyPattern: 'Quit the server with CONTROL-C.'
         },
         upload: {
             target: 'temporary-public-storage',
