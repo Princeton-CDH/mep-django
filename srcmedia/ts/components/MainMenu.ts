21aa58ce740b0e9b5b1afaf9a8cb86d399cdecf1
Nick Budak <nbudak@princeton.edu>
2020-06-03 12:06:06 -0400

    control tabindex for main menu

--- srcmedia/ts/components/MainMenu.ts
+++ srcmedia/ts/components/MainMenu.ts
@@ -23,8 +23,8 @@ export default class MainMenu {
         event.preventDefault()
         this.element.style.zIndex = '100'
         this.element.style.opacity = '1'
-        this.element.style.display = 'block'
         this.element.style.pointerEvents = 'all'
+        this.element.setAttribute('tabindex', '0')
         this.element.removeAttribute('aria-hidden')
         document.body.style.overflowY = 'hidden'
         return new Promise(resolve => setTimeout(resolve, this.transitionDuration))
@@ -40,8 +40,8 @@ export default class MainMenu {
         event.preventDefault()
         this.element.style.zIndex = '-100'
         this.element.style.opacity = '0'
-        this.element.style.display = 'none'
         this.element.style.pointerEvents = 'none'
+        this.element.setAttribute('tabindex', '-1')
         this.element.setAttribute('aria-hidden', 'true')
         document.body.style.overflowY = ''
         return new Promise(resolve => setTimeout(resolve, this.transitionDuration))
