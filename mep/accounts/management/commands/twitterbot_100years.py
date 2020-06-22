685a1ce011944fd56ef39c113b136438a1421d10
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-06-22 10:05:28 -0400

    Revise 100 years twitter review and script logic

--- mep/accounts/management/commands/twitterbot_100years.py
+++ mep/accounts/management/commands/twitterbot_100years.py
@@ -1,9 +1,13 @@
 import datetime
+import subprocess
+import sys
 
 from dateutil.relativedelta import relativedelta
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db.models import Q
 from django.urls import reverse
+import tweepy
 
 from mep.accounts.models import Event
 from mep.accounts.partial_date import DatePrecision
@@ -19,81 +23,194 @@ class Command(BaseCommand):
         DatePrecision.day
 
     def add_arguments(self, parser):
+        parser.add_argument('mode', choices=['report', 'schedule', 'tweet'])
         parser.add_argument(
             '-d', '--date',
-            help='Specify an alternate date in YYYY-MM-DD format (default is today)')
+            help='Specify an alternate date in YYYY-MM-DD format. ' +
+                 '(default is today)')
+        parser.add_argument(
+            '-e', '--event', type=int,
+            help='Database id for the event to be tweeted. ' +
+                 '(Required for tweet mode)')
 
     def handle(self, *args, **kwargs):
-        # TODO: arg to get specific day instead of today
-        # (or date range for reporting?)
-        if kwargs['date']:
-            date = datetime.date(*[int(n) for n in kwargs['date'].split('-')])
+        date = self.get_date(**kwargs)
+
+        if kwargs['mode'] == 'report':
+            self.report(date)
+        elif kwargs['mode'] == 'schedule':
+            self.schedule(date)
+        elif kwargs['mode'] == 'tweet':
+            # todo tweet single event
+            try:
+                ev = Event.objects.get(pk=kwargs['event'])
+                self.tweet(ev, date)
+            except Event.DoesNotExist:
+                self.stderr.write('Error: event %s not found' % kwargs['event'])
+
+    def get_date(self, **kwargs):
+        # find events relative to the specified day if set
+        date = kwargs.get('date', None)
+        # TODO: determine if server time is UTC or local
+
+        # only allow overriding date for report
+        if date and kwargs['mode'] == 'report':
+            relative_date = datetime.date(*[int(n) for n in date.split('-')])
         else:
-            date = datetime.date.today()
+            # by default, report relative to today
+            # determine date 100 years earlier
+            relative_date = datetime.date.today() - relativedelta(years=100)
 
-        # determine date 100 years earlier
-        date = date - relativedelta(years=100)
+        return relative_date
 
-        # find all events for that date
-        # TODO: exclude partially known dates
-        events = Event.objects.filter(start_date=date) \
+    def find_events(self, date):
+        '''Find events 100 years before the current day or
+        a specified day in YYYY-MM-DD format.'''
+
+        # find all events for this date
+        # exclude partially known dates
+        # - purchase date precision == start date precision
+        # (borrow end *could* have different precision than start date)
+        events = Event.objects \
+            .filter(Q(start_date=date) |
+                    Q(subscription__purchase_date=date) |
+                    Q(borrow__isnull=False, end_date=date)) \
             .filter(Q(start_date_precision__isnull=True) |
-                    Q(start_date_precision=int(self.full_precision))) \
+                    Q(start_date_precision=int(self.full_precision)))
+        return events
 
-        print('%s %d event%s' % (date, events.count(),
-                                 's' if events.count() != 1 else ''))
-
-        # TODO: how to group by member?
-        # e.g. joined and borrowed
-        # test with 2020-05-10 and 2020-05-11
-
-        for ev in events:
+    def report(self, date):
+        # needs two modes:
+        # - schedule
+        # - tweet (takes event id)
+        for ev in self.find_events(date):
             print('event type %s' % ev.event_type)
             print('account %s' % ev.account)
             print(ev)
-            content = tweet_content(ev)
+            content = tweet_content(ev, date)
             print(content)
 
+    # times:  9 AM, 12 PM, 1:30 PM, 3 PM, 4:30 PM, 6 PM, 8 PM
+    tweet_times = ['9:00', '12:00', '13:30', '15:00', '16:30', '18:00',
+                   '20:00']
 
-def tweet_content(ev):
-    # TODO: handle multiple events for the same account on the same day?
-    # TODO: handle multiple members
+    def schedule(self, date):
+        # find all events for today
+        self.find_events(date)
+        # filter out any that can't be tweeted
+        events = [ev for ev in self.find_events(date) if can_tweet(ev, date)]
+
+        # schedule the ones that can be tweeted
+        for i, ev in enumerate(events):
+            self.tweet_at(ev, self.tweet_times[i])
+
+    def tweet_at(self, event, time):
+        '''schedule a tweet for later today'''
+        cmd = '%s %s/manage.py twitterbot_100years tweet --event %s' % \
+            (sys.executable, settings.PROJECT_ROOT, event.id)
+        print('scheduling %s at %s' % (event, time))
+        print(cmd)
+        result = subprocess.run(['/usr/bin/at', time], input=cmd.encode(),
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(result.stderr.decode())
+
+    def tweet(self, event, date):
+        # make sure the event is tweetable
+        if not can_tweet(event, date):
+            return
+
+        content = tweet_content(event, date)
+        if not content:
+            return
+        print(content)
+        # TODO: set credentials in local settings
+        # auth = tweepy.OAuthHandler('', '')
+        # auth.set_access_token('', '')
+        # api = tweepy.API(auth)
+        # api.update_status(content)
+
+
+tweetable_event_types = ['Subscription', 'Renewal', 'Reimbursement',
+                         'Borrow', 'Purchase', 'Request']
+
+
+def can_tweet(ev, date):
+    '''check if the event can be tweeted on the specified day'''
+
+    # convert to string and compare against partial dates
+    # to ensure we don't tweet an event with an unknown date
+    date = date.isoformat()
+
+    # disallows subscription on start date != purchase
+    if ev.event_label == 'Subscription' and \
+       ev.subscription.partial_purchase_date != date:
+        return False
+
+    return any([
+        # subscription event and date matches purchase
+        (ev.event_label == 'Subscription' and
+         ev.subscription.partial_purchase_date == date),
+        # borrow event and date matches end date
+        (ev.event_label == 'Borrow' and ev.partial_end_date == date),
+        # any other tweetable event and date matches start
+        ev.event_label in tweetable_event_types and
+        ev.partial_start_date == date])
+
+
+def tweet_content(ev, event_date):
+    # handle multiple members, but use first member for url
     member = ev.account.persons.first()
+
+    if isinstance(event_date, str):
+        try:
+            event_date = datetime.date(*[int(n) for n in event_date.split('-')])
+        except TypeError:
+            # given a partial date
+            return
+
+    member_name = ' and '.join(m.name for m in ev.account.persons.all())
     prolog = '#100YearsAgoToday on %s:' % \
-        ev.start_date.strftime(Command.date_format)
+        event_date.strftime(Command.date_format)
     content = None
 
-    if ev.event_type == 'Subscription':
-        content = '%s joined the Shakespeare and Company lending library.' % \
-            member.name
+    event_label = ev.event_label
 
-    elif ev.event_type in ['Borrow', 'Purchase']:
+    if event_label == 'Subscription' and \
+       ev.subscription.purchase_date == event_date:
+        content = '%s joined the Shakespeare and Company lending library.' % \
+            member_name
+
+    elif event_label in ['Borrow', 'Purchase', 'Request']:
         work = work_label(ev.work)
+        # convert event type into verb for the sentence
         verb = '%sed' % ev.event_type.lower().rstrip('e')
-        content = '%s %s %s.' % (member.name, verb, work)
+        if event_label == 'Borrow' and ev.end_date == event_date:
+            verb = 'returned'
+        content = '%s %s %s.' % (member_name, verb, work)
 
     # renewal
-    elif ev.event_type == 'Renewal':
+    elif event_label == 'Renewal' and ev.subscription.purchase_date == event_date:
         # renewed for 2 months at 1 volume per month
         content = '%s renewed for %s' % \
-            (member.name, ev.subscription.readable_duration())
+            (member_name, ev.subscription.readable_duration())
         # include volume count if known
         if ev.subscription.volumes:
             content = '%s at %d volume%s per month.' % \
                 (content, ev.subscription.volumes,
                  '' if ev.subscription.volumes == 1 else 's')
 
-    # probably don't post these...
-    elif ev.event_type == 'Generic':
-        print(ev.notes)
+    elif event_label == 'Reimbursement':
+        # renewed for 2 months at 1 volume per month
+        content = '%s received a reimbursement for %s%s from the Shakespeare and Company lending library' % \
+            (member_name, ev.reimbursement.refund,
+             ev.reimbursement.currency_symbol())
+
+    # any other kind of event: no tweet
 
     if content:
         # change: use card detail url if possible
         url = card_url(member, ev) or member.get_absolute_url()
         return '%s %s\n%s' % (prolog, content, absolutize_url(url))
-
-    # TODO: add logic to determine schedule and actually schedule
-    # tweets for the current day
 
 
 def work_label(work):
--- mep/accounts/views.py
+++ mep/accounts/views.py
@@ -1,4 +1,5 @@
 import datetime
+from collections import defaultdict
 
 from dal import autocomplete
 from dateutil.relativedelta import relativedelta
@@ -38,12 +39,46 @@ class Twitter100yearsReview(LoginRequiredOr404Mixin, ListView):
 
     def get_queryset(self):
         date = datetime.date.today()
+
         # determine date 100 years earlier
-        date = date - relativedelta(years=100)
-        two_weeks_ahead = date + relativedelta(weeks=4)
-        return super().get_queryset() \
-            .filter(start_date__gte=date,
-                    start_date__lte=two_weeks_ahead) \
+        date_start = date - relativedelta(years=100)
+        # determine end date for tweets to review
+        date_end = date_start + relativedelta(weeks=4)
+
+        events = Event.objects \
+            .filter(Q(start_date__gte=date_start,
+                      start_date__lte=date_end) |
+                    Q(subscription__purchase_date__gte=date_start,
+                      subscription__purchase_date__lte=date_end) |
+                    Q(borrow__isnull=False, end_date__gte=date_start,
+                      end_date__lte=date_end)) \
             .filter(Q(start_date_precision__isnull=True) |
-                    Q(start_date_precision=int(self.full_precision))) \
-            .order_by('start_date', 'account')
+                    Q(start_date_precision=int(self.full_precision)))
+
+        return events
+
+    def get_context_data(self):
+        context = super().get_context_data()
+
+        # construct a dictionary of dates with a list of events,
+        # to make it easy to display all tweets in order
+        events_by_date = defaultdict(list)
+
+        for ev in self.object_list:
+            if ev.event_label in ['Subscription', 'Renewal']:
+                events_by_date[ev.subscription.partial_purchase_date].append(ev)
+            elif ev.event_label == 'Borrow':
+                events_by_date[ev.partial_start_date].append(ev)
+                events_by_date[ev.partial_end_date].append(ev)
+            else:
+                events_by_date[ev.partial_start_date].append(ev)
+
+        # could include unset dates; remove them
+        # NOTE: could include dates out of the current range; do we care?
+        del events_by_date[None]
+
+        # convert to a standard dict to avoid problems with django templates;
+        # sort by date so review will be sequential
+        events_by_date = {k: events_by_date[k] for k in sorted(events_by_date)}
+        context['events_by_date'] = events_by_date
+        return context
