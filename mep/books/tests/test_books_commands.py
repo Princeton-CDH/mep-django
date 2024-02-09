88c2d0ec105dc68c3458b8cf06cb5c844871f319
Ryan Heuser <ryan.heuser@princeton.edu>
2024-02-09 10:41:38 -0500

    book tests

--- mep/books/tests/test_books_commands.py
+++ mep/books/tests/test_books_commands.py
@@ -406,6 +406,7 @@ class TestExportBooks(TestCase):
         assert data["uri"] == absolutize_url(exit_e.get_absolute_url())
         assert data["title"] == exit_e.title
         assert data["year"] == exit_e.year
+        assert data["genre_category"] == exit_e.category.name
         assert data["format"] == exit_e.work_format.name
         assert not data["uncertain"]  # not marked uncertain
         assert "work_uri" not in data
