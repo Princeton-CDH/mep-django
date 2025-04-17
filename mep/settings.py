7d0e4a684765ec7009ef8df010211be22e58c9ab
Ben Silverman <ben@performantsoftware.com>
2025-04-17 14:47:05 -0400

    Run unit tests under Python 3.10+ (#843)

--- mep/settings.py
+++ mep/settings.py
@@ -293,10 +293,10 @@ CSP_INCLUDE_NONCE_IN = ("script-src",)
 f = os.path.join(BASE_DIR, "mep", "local_settings.py")
 if os.path.exists(f):
     import sys
-    import imp
+    import types
 
     module_name = "mep.local_settings"
-    module = imp.new_module(module_name)
+    module = types.ModuleType(module_name)
     module.__file__ = f
     sys.modules[module_name] = module
     exec(open(f, "rb").read())
